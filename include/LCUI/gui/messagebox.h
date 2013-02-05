5c098eca417e2bcaa5b10c570dda9bcfeef75374
Liu Chao <lc-soft@live.cn>
2013-02-05 16:15:52 +0800

    完善MessageBox的代码

--- include/LCUI/gui/messagebox.h
+++ include/LCUI/gui/messagebox.h
@@ -31,9 +31,38 @@ typedef enum {
 
 LCUI_BEGIN_HEADER
 
+/* LCUI的消息框
+ * 参数：
+ * icon_type	: 指定消息框中显示的图标，可取以下值：
+ * 	MB_ICON_NONE	无图标
+ * 	MB_ICON_ERROR	“错误”图标
+ * 	MB_ICON_HELP	”帮助“图标
+ * 	MB_ICON_INFO	“信息”图标
+ * 	MB_ICON_WARNING	“警告”图标
+ * text		: 消息框内显示的消息内容
+ * title	: 消息框的标题文本
+ * button	: 指定消息框内显示的按钮，可取以下值：
+ * 	MB_BTN_OK		: 一个“确定”按钮
+ * 	MB_BTN_OKCANCEL		: “确定”和“取消”按钮
+ * 	MB_BTN_ABORTRETRYIGNORE	: “终止”、"重试"和“忽略”按钮
+ * 	MB_BTN_RETRYCANCEL	: "重试"和“取消”按钮
+ * 	MB_BTN_YESNO		: "是"和“否”按钮
+ * 	MB_BTN_YESNOCANCEL	: “是”、"否"和“取消”按钮
+ * 返回值：
+ * -1	: button参数的值异常
+ * -2	: 无法获取消息框关闭时所点击的按钮ID
+ * 正常则会返回按钮的ID，有以下值：
+ * 	MB_BTN_IS_OK	: "确定"按钮	
+ * 	MB_BTN_IS_CANCEL: "取消"按钮
+ * 	MB_BTN_IS_ABORT	: "终止"按钮
+ * 	MB_BTN_IS_RETRY	: "重试"按钮
+ * 	MB_BTN_IS_IGNORE: "忽略"按钮
+ * 	MB_BTN_IS_YES	: "是"按钮
+ * 	MB_BTN_IS_NO	: "否"按钮
+ * 	MB_BTN_IS_QUIT	: 窗口右上角的关闭按钮
+ *  */
 int LCUI_MessageBox(	MB_ICON_TYPE icon_type, const char *text, 
 			const char *title, MB_BTN_TYPE button );
-
 LCUI_END_HEADER
 
 #endif
--- src/gui/messagebox.c
+++ src/gui/messagebox.c
@@ -118,9 +118,8 @@ msgbox_mainloop_quit( LCUI_Widget *btn, LCUI_WidgetEvent *unused )
 static void
 auto_resize_msgbox(LCUI_Widget *textlabel, LCUI_WidgetEvent *event )
 {
-	int area_height, min_width;
+	int area_height;
 	LCUI_Widget *msgbox, *msg_area;
-	MB_BTN_TYPE btn_type;
 	LCUI_Size msgbox_size;
 	
 	msgbox = Widget_GetParent( textlabel, "window" );
@@ -130,51 +129,36 @@ auto_resize_msgbox(LCUI_Widget *textlabel, LCUI_WidgetEvent *event )
 	if( (msgbox->self_id & WIDGET_ID_MESSAGEBOX) != WIDGET_ID_MESSAGEBOX ) {
 		return;
 	}
-	btn_type = msgbox->self_id ^ WIDGET_ID_MESSAGEBOX;
-	switch( btn_type ) {
-	case MB_BTN_OK:
-		min_width = 150;
-		break;
-	case MB_BTN_YESNO:
-	case MB_BTN_RETRYCANCEL:
-	case MB_BTN_OKCANCEL:
-		min_width = 180;
-		break;
-	case MB_BTN_YESNOCANCEL:
-	case MB_BTN_ABORTRETRYIGNORE:
-		min_width = 210;
-		break;
-	default: return;
-	}
+	/* 以下只是估算消息框的尺寸，不要纠结 为何要+=14、为何要+=30 等之类的问题 */
 	if( (msgbox->self_id & MSGBOX_NONE_ICON) == MSGBOX_NONE_ICON ) {
-		msgbox_size.h = msgbox_size.w = area_height = 10;
+		msgbox_size.h = area_height = MB_MSG_AREA_HEIGHT;
+		msgbox_size.w = 0;
 	} else {
 		msgbox_size = MB_ICONBOX_SIZE;
-		msgbox_size.w += 14;
-		msgbox_size.w += event->resize.new_size.w;
-		area_height = msgbox_size.h;
-		if( area_height < event->resize.new_size.h ) {
-			area_height = event->resize.new_size.h;
-		}
-		if( area_height < MB_MSG_AREA_HEIGHT ) {
-			area_height = MB_MSG_AREA_HEIGHT;
-		} else {
-			_DEBUG_MSG("area_height: %d\n", area_height);
-		}
-		/* 加上内容区域的上下内边距的5像素 */
-		area_height += 5;
-		/* 加上按钮区域的固定高度 */
-		msgbox_size.h = area_height + MB_BTN_AREA_HEIGHT;
 	}
+	msgbox_size.w += 14;
+	msgbox_size.w += event->resize.new_size.w;
+	area_height = msgbox_size.h;
+	if( area_height < event->resize.new_size.h ) {
+		area_height = event->resize.new_size.h;
+	}
+	if( area_height < MB_MSG_AREA_HEIGHT ) {
+		area_height = MB_MSG_AREA_HEIGHT;
+	}
+	
+	_DEBUG_MSG("area_height: %d\n", area_height);
+	/* 加上按钮区域的固定高度 */
+	msgbox_size.h = area_height + MB_BTN_AREA_HEIGHT;
 	/* 加上窗口边框尺寸 */
 	msgbox_size.w += 14;
 	msgbox_size.h += 30;
 
-	_DEBUG_MSG("widget: %p, btn_type: %d, old size: (%d,%d), new size: (%d,%d)\n",
-		textlabel, btn_type, 
+	_DEBUG_MSG("widget: %p, old size: (%d,%d), new size: (%d,%d)\n",
+		textlabel, 
 		event->resize.old_size.w, event->resize.old_size.h, 
 		event->resize.new_size.w, event->resize.new_size.h);
-	_DEBUG_MSG("msgbox size: (%d,%d)\n",msgbox_size.w, msgbox_size.h);
+	_DEBUG_MSG("msgbox size: (%d,%d)\n", msgbox_size.w, msgbox_size.h);
+	
 	/* 获取消息框内的消息区域的指针 */
 	msg_area = Widget_GetChildByID(	Window_GetClientArea(msgbox), 
 					WIDGET_ID_MB_MSG_AREA );
@@ -201,9 +185,29 @@ msgbox_add_BTN_OK( LCUI_Widget *btn_area )
 }
 
 static void
-msgbox_add_BTN_OKCANCEL( LCUI_Widget *msgbox )
+msgbox_add_BTN_OKCANCEL( LCUI_Widget *btn_area )
 {
-	
+	LCUI_Size btn_size;
+	LCUI_Widget *btn[2];
+	btn[0] = Widget_New("button");
+	btn[1] = Widget_New("button");
+	btn[0]->self_id = MB_BTN_IS_OK;
+	btn[1]->self_id = MB_BTN_IS_CANCEL;
+	Widget_Resize( btn[0], MB_BTN_SIZE );
+	Widget_Resize( btn[1], MB_BTN_SIZE );
+	Widget_SetAutoSize( btn[0], FALSE, 0 );
+	Widget_SetAutoSize( btn[1], FALSE, 0 );
+	Set_Button_Text( btn[0], TEXT_MB_BTN_OK );
+	Set_Button_Text( btn[1], TEXT_MB_BTN_CANCEL );
+	Widget_Container_Add( btn_area, btn[0] );
+	Widget_Container_Add( btn_area, btn[1] );
+	btn_size = MB_BTN_SIZE;
+	Widget_SetAlign( btn[0], ALIGN_MIDDLE_CENTER, Pos(0-(btn_size.w/2.0+3),0) );
+	Widget_SetAlign( btn[1], ALIGN_MIDDLE_CENTER, Pos((btn_size.w/2.0+3),0) );
+	Widget_Event_Connect( btn[0], EVENT_CLICKED, msgbox_mainloop_quit );
+	Widget_Event_Connect( btn[1], EVENT_CLICKED, msgbox_mainloop_quit );
+	Widget_Show( btn[0] );
+	Widget_Show( btn[1] );
 }
 
 static void
@@ -242,24 +246,122 @@ msgbox_add_BTN_ABORTRETRYIGNORE( LCUI_Widget *btn_area )
 }
 
 static void
-msgbox_add_BTN_RETRYCANCEL( LCUI_Widget *msgbox )
+msgbox_add_BTN_RETRYCANCEL( LCUI_Widget *btn_area )
 {
-	
+	LCUI_Size btn_size;
+	LCUI_Widget *btn[3];
+	btn[0] = Widget_New("button");
+	btn[1] = Widget_New("button");
+	btn[0]->self_id = MB_BTN_IS_RETRY;
+	btn[1]->self_id = MB_BTN_IS_CANCEL;
+	Widget_Resize( btn[0], MB_BTN_SIZE );
+	Widget_Resize( btn[1], MB_BTN_SIZE );
+	Widget_SetAutoSize( btn[0], FALSE, 0 );
+	Widget_SetAutoSize( btn[1], FALSE, 0 );
+	Set_Button_Text( btn[0], TEXT_MB_BTN_RETRY );
+	Set_Button_Text( btn[1], TEXT_MB_BTN_CANCEL );
+	Widget_Container_Add( btn_area, btn[0] );
+	Widget_Container_Add( btn_area, btn[1] );
+	btn_size = MB_BTN_SIZE;
+	Widget_SetAlign( btn[0], ALIGN_MIDDLE_CENTER, Pos(0-(btn_size.w/2.0+3),0) );
+	Widget_SetAlign( btn[1], ALIGN_MIDDLE_CENTER, Pos((btn_size.w/2.0+3),0) );
+	Widget_Event_Connect( btn[0], EVENT_CLICKED, msgbox_mainloop_quit );
+	Widget_Event_Connect( btn[1], EVENT_CLICKED, msgbox_mainloop_quit );
+	Widget_Show( btn[0] );
+	Widget_Show( btn[1] );
 }
 
 static void
-msgbox_add_BTN_YESNO( LCUI_Widget *msgbox )
+msgbox_add_BTN_YESNO( LCUI_Widget *btn_area )
 {
-	
+	LCUI_Size btn_size;
+	LCUI_Widget *btn[2];
+	btn[0] = Widget_New("button");
+	btn[1] = Widget_New("button");
+	btn[0]->self_id = MB_BTN_IS_YES;
+	btn[1]->self_id = MB_BTN_IS_NO;
+	Widget_Resize( btn[0], MB_BTN_SIZE );
+	Widget_Resize( btn[1], MB_BTN_SIZE );
+	Widget_SetAutoSize( btn[0], FALSE, 0 );
+	Widget_SetAutoSize( btn[1], FALSE, 0 );
+	Set_Button_Text( btn[0], TEXT_MB_BTN_YES );
+	Set_Button_Text( btn[1], TEXT_MB_BTN_NO );
+	Widget_Container_Add( btn_area, btn[0] );
+	Widget_Container_Add( btn_area, btn[1] );
+	btn_size = MB_BTN_SIZE;
+	Widget_SetAlign( btn[0], ALIGN_MIDDLE_CENTER, Pos(0-(btn_size.w/2.0+3),0) );
+	Widget_SetAlign( btn[1], ALIGN_MIDDLE_CENTER, Pos((btn_size.w/2.0+3),0) );
+	Widget_Event_Connect( btn[0], EVENT_CLICKED, msgbox_mainloop_quit );
+	Widget_Event_Connect( btn[1], EVENT_CLICKED, msgbox_mainloop_quit );
+	Widget_Show( btn[0] );
+	Widget_Show( btn[1] );
 }
 
 static void
-msgbox_add_BTN_YESNOCANCEL( LCUI_Widget *msgbox )
+msgbox_add_BTN_YESNOCANCEL( LCUI_Widget *btn_area )
 {
-	
+	LCUI_Size btn_size;
+	LCUI_Widget *btn[3];
+	btn[0] = Widget_New("button");
+	btn[1] = Widget_New("button");
+	btn[2] = Widget_New("button");
+	btn[0]->self_id = MB_BTN_IS_YES;
+	btn[1]->self_id = MB_BTN_IS_NO;
+	btn[2]->self_id = MB_BTN_IS_CANCEL;
+	Widget_Resize( btn[0], MB_BTN_SIZE );
+	Widget_Resize( btn[1], MB_BTN_SIZE );
+	Widget_Resize( btn[2], MB_BTN_SIZE );
+	Widget_SetAutoSize( btn[0], FALSE, 0 );
+	Widget_SetAutoSize( btn[1], FALSE, 0 );
+	Widget_SetAutoSize( btn[2], FALSE, 0 );
+	Set_Button_Text( btn[0], TEXT_MB_BTN_YES );
+	Set_Button_Text( btn[1], TEXT_MB_BTN_NO );
+	Set_Button_Text( btn[2], TEXT_MB_BTN_CANCEL );
+	Widget_Container_Add( btn_area, btn[0] );
+	Widget_Container_Add( btn_area, btn[1] );
+	Widget_Container_Add( btn_area, btn[2] );
+	btn_size = MB_BTN_SIZE;
+	Widget_SetAlign( btn[0], ALIGN_MIDDLE_CENTER, Pos(0-(btn_size.w+3),0) );
+	Widget_SetAlign( btn[1], ALIGN_MIDDLE_CENTER, Pos(0,0) );
+	Widget_SetAlign( btn[2], ALIGN_MIDDLE_CENTER, Pos((btn_size.w+3),0) );
+	Widget_Event_Connect( btn[0], EVENT_CLICKED, msgbox_mainloop_quit );
+	Widget_Event_Connect( btn[1], EVENT_CLICKED, msgbox_mainloop_quit );
+	Widget_Event_Connect( btn[2], EVENT_CLICKED, msgbox_mainloop_quit );
+	Widget_Show( btn[0] );
+	Widget_Show( btn[1] );
+	Widget_Show( btn[2] );
 }
 
-
+/* LCUI的消息框
+ * 参数：
+ * icon_type	: 指定消息框中显示的图标，可取以下值：
+ * 	MB_ICON_NONE	无图标
+ * 	MB_ICON_ERROR	“错误”图标
+ * 	MB_ICON_HELP	”帮助“图标
+ * 	MB_ICON_INFO	“信息”图标
+ * 	MB_ICON_WARNING	“警告”图标
+ * text		: 消息框内显示的消息内容
+ * title	: 消息框的标题文本
+ * button	: 指定消息框内显示的按钮，可取以下值：
+ * 	MB_BTN_OK		: 一个“确定”按钮
+ * 	MB_BTN_OKCANCEL		: “确定”和“取消”按钮
+ * 	MB_BTN_ABORTRETRYIGNORE	: “终止”、"重试"和“忽略”按钮
+ * 	MB_BTN_RETRYCANCEL	: "重试"和“取消”按钮
+ * 	MB_BTN_YESNO		: "是"和“否”按钮
+ * 	MB_BTN_YESNOCANCEL	: “是”、"否"和“取消”按钮
+ * 返回值：
+ * -1	: button参数的值异常
+ * -2	: 无法获取消息框关闭时所点击的按钮ID
+ * 正常则会返回按钮的ID，有以下值：
+ * 	MB_BTN_IS_OK	: "确定"按钮	
+ * 	MB_BTN_IS_CANCEL: "取消"按钮
+ * 	MB_BTN_IS_ABORT	: "终止"按钮
+ * 	MB_BTN_IS_RETRY	: "重试"按钮
+ * 	MB_BTN_IS_IGNORE: "忽略"按钮
+ * 	MB_BTN_IS_YES	: "是"按钮
+ * 	MB_BTN_IS_NO	: "否"按钮
+ * 	MB_BTN_IS_QUIT	: 窗口右上角的关闭按钮
+ *  */
 int LCUI_MessageBox(	MB_ICON_TYPE icon_type, const char *text, 
 			const char *title, MB_BTN_TYPE button )
 {
@@ -280,6 +382,7 @@ int LCUI_MessageBox(	MB_ICON_TYPE icon_type, const char *text,
 	msg_area->self_id = WIDGET_ID_MB_MSG_AREA;
 	
 	Graph_Init( &icon );
+	/* 根据参数的值，载入相应的图标 */
 	switch(icon_type) {
 	case MB_ICON_NONE:break;
 	case MB_ICON_ERROR:
@@ -295,47 +398,51 @@ int LCUI_MessageBox(	MB_ICON_TYPE icon_type, const char *text,
 		Load_Graph_Icon_Warning( &icon );
 		break;
 	}
+	/* 添加图标后文本进消息区域 */
 	Widget_Container_Add( msg_area, iconbox );
 	Widget_Container_Add( msg_area, textbox );
+	/* 设定图标框的默认尺寸 */
 	Widget_Resize( iconbox, MB_ICONBOX_SIZE );
+	/* 为图标框设置需显示的图标 */
 	Set_PictureBox_Image_From_Graph( iconbox, &icon );
 	Set_PictureBox_Size_Mode( iconbox, SIZE_MODE_CENTER );
+	/* 判断是否有图标，并调整文本位置 */
 	if( icon_type == MB_ICON_NONE ) {
 		icon_flag = MSGBOX_NONE_ICON;
-		Widget_SetAlign( textbox, ALIGN_MIDDLE_LEFT, Pos(0,0) );
+		Widget_SetAlign( textbox, ALIGN_MIDDLE_LEFT, Pos(5,0) );
 	} else {
 		icon_flag = 0;
 		Widget_SetAlign( iconbox, ALIGN_MIDDLE_LEFT, Pos(5,0) );
 		Widget_SetAlign( textbox, ALIGN_MIDDLE_LEFT, 
 				Pos(icon.width + 10,0) );
 	}
-	
+	/* 根据按钮数量，得出相应的最小尺寸 */
 	switch( button ) {
 	case MB_BTN_OK:
 		msgbox_add_BTN_OK( btn_area );
-		min_size = Size(150,130);
+		min_size = Size(150,125);
 		break;
 	case MB_BTN_OKCANCEL:
 		msgbox_add_BTN_OKCANCEL( btn_area );
-		min_size = Size(180,130);
+		min_size = Size(180,125);
 		break;
 	case MB_BTN_ABORTRETRYIGNORE:
 		msgbox_add_BTN_ABORTRETRYIGNORE( btn_area );
-		min_size = Size(215,130);
+		min_size = Size(215,125);
 		break;
 	case MB_BTN_RETRYCANCEL:
 		msgbox_add_BTN_RETRYCANCEL( btn_area );
-		min_size = Size(180,130);
+		min_size = Size(180,125);
 		break;
 	case MB_BTN_YESNO:
 		msgbox_add_BTN_YESNO( btn_area );
-		min_size = Size(180,130);
+		min_size = Size(180,125);
 		break;
 	case MB_BTN_YESNOCANCEL:
 		msgbox_add_BTN_YESNOCANCEL( btn_area );
-		min_size = Size(215,130);
+		min_size = Size(215,125);
 		break;
-	default: break;
+	default: return -1;
 	}
 	/* 创建消息框，并设置ID */
 	msgbox = Window_New( title, NULL, min_size );
@@ -356,6 +463,7 @@ int LCUI_MessageBox(	MB_ICON_TYPE icon_type, const char *text,
 	Widget_SetDock( btn_area, DOCK_TYPE_BOTTOM );
 	Widget_SetSize( msg_area, NULL, MB_MSG_AREA_HEIGHT_TEXT );
 	Widget_SetSize( btn_area, NULL, MB_BTN_AREA_HEIGHT_TEXT );
+	
 	/* 设定按钮区域的样式 */
 	Widget_SetBackgroundTransparent( btn_area, FALSE );
 	Widget_SetBackgroundColor( btn_area, MB_BTN_AREA_BGCOLOR );
@@ -388,5 +496,5 @@ int LCUI_MessageBox(	MB_ICON_TYPE icon_type, const char *text,
 		msgbox_data_delete( msgbox );
 		return ret;
 	}
-	return -1;
+	return -2;
 }
