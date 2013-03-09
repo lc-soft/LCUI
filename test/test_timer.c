fb0c8d419807b8d670b0abcec2d35f2b186e2e78
Liu Chao <lc-soft@live.cn>
2013-03-09 22:03:23 +0800

    更新定时器测试程序

--- test/test_timer.c
+++ test/test_timer.c
@@ -7,40 +7,46 @@
 #include LC_LABEL_H
 #include <unistd.h>
 
-static int status = 0;
+static int state = 0;
 static LCUI_Widget *label;
 
-void display_text()
+static void destroy( LCUI_Widget *widget, LCUI_WidgetEvent *unused )
 {
-	if( status == 0 ) {
-		Show_Widget( label );
-		status = 1;
+	LCUI_MainLoop_Quit(NULL);
+}
+
+static void display_text(void)
+{
+	if( state == 0 ) {
+		Widget_Show( label );
+		state = 1;
 	} else {
-		Hide_Widget( label );
-		status = 0;
+		Widget_Hide( label );
+		state = 0;
 	}
 }
 
-int main(int argc, char*argv[]) 
+int main(void) 
 {
-        LCUI_Widget *window;
-        LCUI_TextStyle style;
-        
-        LCUI_Init(argc, argv);
-        TextStyle_Init( &style );
-        
-        window  = Create_Widget("window");
-        label   = Create_Widget("label");
-        
-        Set_Window_Title_Text(window, "测试定时器");
-        window->resize(window, Size(320, 240));
-        Window_Client_Area_Add(window, label);
-        label->set_align(label, ALIGN_MIDDLE_CENTER, Pos(0,0));
-        Label_Text(label, "利用定时器实现的文本闪烁");
-        TextStyle_FontSize( &style, 20 );
-        Label_TextStyle( label, style );
-        window->show( window ); 
-        /* 设置定时器，每隔500毫秒调用display_text函数，重复调用 */
-        set_timer( 500, display_text, TRUE );
-        return LCUI_Main(); /* 进入主循环 */
+	LCUI_Widget *window;
+	LCUI_TextStyle style;
+	
+	LCUI_Init();
+	TextStyle_Init( &style );
+	
+	window  = Widget_New("window");
+	label   = Widget_New("label");
+	
+	Window_SetTitleText( window, "测试定时器" );
+	Widget_Resize( window, Size(320, 240) );
+	Window_ClientArea_Add( window, label );
+	Widget_SetAlign( label, ALIGN_MIDDLE_CENTER, Pos(0,0) );
+	Label_Text( label, "利用定时器实现的文本闪烁" );
+	TextStyle_FontSize( &style, 20 );
+	Label_TextStyle( label, style );
+	Widget_Event_Connect( Window_GetCloseButton(window), EVENT_CLICKED, destroy );
+	/* 设置定时器，每隔500毫秒调用display_text函数，重复调用 */
+	set_timer( 500, display_text, TRUE );
+	Widget_Show( window ); 
+	return LCUI_Main();
 }
