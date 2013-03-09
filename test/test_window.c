517822484c7bc73ca0c83aadc6229bcd99070cdc
Liu Chao <lc-soft@live.cn>
2013-03-09 22:00:51 +0800

    更新窗口测试程序

--- test/test_window.c
+++ test/test_window.c
@@ -6,15 +6,19 @@
 #include LC_RADIOBTN_H
 #include LC_BUTTON_H
 #include LC_LABEL_H
-#include <unistd.h>
+
+static void destroy( LCUI_Widget *widget, LCUI_WidgetEvent *unused )
+{
+	LCUI_MainLoop_Quit(NULL);
+}
 
 //#define TEST_THIS
 #ifdef TEST_THIS
-/* 声明一些部件 */
+
 static LCUI_Widget *main_window, *label, *btn_ok, *rdbtn[5];
 
 /* 按下按钮后将调用的回调函数 */
-void switch_style(LCUI_Widget *widget, void *arg)
+void switch_style(LCUI_Widget *widget, LCUI_WidgetEvent *event)
 {
 	/* 找到被选中的单选框，并为窗口设定相应风格 */
 	if(RadioButton_IsOn(rdbtn[0])) {
@@ -34,14 +38,13 @@ void switch_style(LCUI_Widget *widget, void *arg)
 	}
 }
 
-int main(int argc, char *argv[]) 
+int main(void) 
 {
-	/* LCUI初始化 */
-	LCUI_Init(argc, argv);
-	
 	int i; 
 	char text[5][10] = { "蓝色","绿色", "红色", "橙色", "紫色" };
-	LCUI_Pos offset_pos[5]={{-120,0},{-60,0},{0,0},{60,0},{120,0}};
+	LCUI_Pos offset_pos[5]={{-120,0},{-60,0},{0,0},{60,0},{120,0}};	
+
+	LCUI_Init();
 	
 	main_window = Widget_New( "window" ); 
 	btn_ok = Widget_New( "button" );
@@ -54,7 +57,7 @@ int main(int argc, char *argv[])
 		rdbtn[i] = Widget_New( "radio_button" );
 		Window_ClientArea_Add( main_window, rdbtn[i] );
 		Widget_SetAlign( rdbtn[i], ALIGN_MIDDLE_CENTER, offset_pos[i] );
-		RadioButton_Text( rdbtn[i], "%s", text[i] );
+		RadioButton_Text( rdbtn[i], text[i] );
 		Widget_Show( rdbtn[i] );
 	}
 	/* 为单选框建立互斥关系 */
@@ -76,7 +79,8 @@ int main(int argc, char *argv[])
 	Button_Text( btn_ok, "应用" ); 
 	Label_Text( label, "选择窗口配色：" );
 	/* 为按钮的点击事件关联回调函数 */
-	Widget_ClickedEvent_Connect( btn_ok, switch_style, NULL );
+	Widget_Event_Connect( btn_ok, EVENT_CLICKED, switch_style );
+	Widget_Event_Connect( Window_GetCloseButton(main_window), EVENT_CLICKED, destroy );
 	/* 显示部件 */
 	Widget_Show( btn_ok );
 	Widget_Show( label );
@@ -87,15 +91,10 @@ int main(int argc, char *argv[])
 
 #else
 
-static void callback( LCUI_Widget *widget, LCUI_WidgetEvent *unused )
-{
-	LCUI_MainLoop_Quit(NULL);
-}
-
 int main(int argc, char*argv[])
 /* 主函数，程序的入口 */
 {
-	LCUI_Init(argc, argv);
+	LCUI_Init();
 	LCUI_Widget *windows[4];
 	/* 创建窗口部件 */
 	windows[0] = Window_New("主窗口", NULL, Size(320, 240));
@@ -115,7 +114,7 @@ int main(int argc, char*argv[])
 	Widget_Show(windows[1]); 
 	Widget_Show(windows[2]);
 	Widget_Show(windows[3]);
-	Widget_Event_Connect( Window_GetCloseButton(windows[0]), EVENT_CLICKED, callback );
+	Widget_Event_Connect( Window_GetCloseButton(windows[0]), EVENT_CLICKED, destroy);
 	LCUI_Main(); /* 进入主循环 */ 
 	return 0;
 }
