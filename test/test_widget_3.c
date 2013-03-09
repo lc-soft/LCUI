1fcc01986d2ece02c38d52816d3b3487da164bb2
Liu Chao <lc-soft@live.cn>
2013-03-09 22:01:30 +0800

    更新 部件内边距 测试程序

--- test/test_widget_3.c
+++ test/test_widget_3.c
@@ -7,39 +7,51 @@
 #include LC_WINDOW_H
 #include LC_LABEL_H
 #include LC_GRAPH_H
-#include <unistd.h>
 
-int main(int argc, char *argv[])
+LCUI_Widget *area;
+
+static void destroy( LCUI_Widget *widget, LCUI_WidgetEvent *unused )
+{
+	LCUI_MainLoop_Quit(NULL);
+}
+
+int main(void)
 {
 	int i;
-	LCUI_Init( argc, argv );
+	char str[20];
 	LCUI_RGB color[6]={{255,255,255},{127,127,127}, {255,0,0}, {255,165,0},
 			{30,144,255}, {65,200,65} };
 	LCUI_Widget *window, *widget[6], *label[6];
 	
-	window = Create_Widget( "window" );
+	LCUI_Init();
+	window = Widget_New( "window" );
+	area = Widget_New(NULL);
 	/* 设置客户区的内边距 */
-	Set_Widget_Padding( Get_Window_Client_Area(window), Padding(15,5,10,20) );
+	Widget_SetPadding( area, Padding(15,5,10,20) );
+	Window_ClientArea_Add( window, area );
+	Widget_SetDock( area, DOCK_TYPE_FILL );
 	for(i=0; i<6; ++i) {
-		widget[i] = Create_Widget( NULL );
-		label[i] = Create_Widget( "label" );
-		Label_Text( label[i], "%c" , 'A'+i);
+		widget[i] = Widget_New( NULL );
+		label[i] = Widget_New( "label" );
+		sprintf( str, "%c" , 'A'+i );
+		Label_Text( label[i], str );
 		Widget_Container_Add( widget[i], label[i] );
-		Window_Client_Area_Add( window, widget[i] );
-		Resize_Widget( widget[i], Size(80,80) );
-		Set_Widget_PosType( widget[i], POS_TYPE_STATIC );
-		Set_Widget_Align( label[i], ALIGN_MIDDLE_CENTER, Pos(0,0) );
-		Set_Widget_Border( widget[i], RGB(0,0,0), Border(1,1,1,1) );
-		Set_Widget_BG_Mode( widget[i], BG_MODE_FILL_BACKCOLOR );
-		Set_Widget_Backcolor( widget[i], color[i] );
-		Show_Widget( label[i] );
-		Show_Widget( widget[i] );
+		Widget_Container_Add( area, widget[i] );
+		Widget_Resize( widget[i], Size(80,80) );
+		Widget_SetPosType( widget[i], POS_TYPE_STATIC );
+		Widget_SetAlign( label[i], ALIGN_MIDDLE_CENTER, Pos(0,0) );
+		Widget_SetBorder( widget[i], Border(1,BORDER_STYLE_SOLID,RGB(0,0,0)) );
+		Widget_SetBackgroundTransparent( widget[i], FALSE );
+		Widget_SetBackgroundColor( widget[i], color[i] );
+		Widget_Show( label[i] );
+		Widget_Show( widget[i] );
 	}
 	
-	Set_Window_Title_Text( window, "测试窗口客户区的内边距属性" ); 
-	Resize_Widget( window, Size(320, 240) );
-	
-	Show_Widget( window );
+	Window_SetTitleText( window, "测试窗口客户区的内边距属性" ); 
+	Widget_Resize( window, Size(320, 240) );
+	Widget_Event_Connect( Window_GetCloseButton(window), EVENT_CLICKED, destroy );
+	Widget_Show( area );
+	Widget_Show( window );
 	
 	return LCUI_Main();
 }
