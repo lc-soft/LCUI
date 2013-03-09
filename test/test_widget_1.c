217dc338cb801a4e8f6f53c21ff9934d01b7cb2a
Liu Chao <lc-soft@live.cn>
2013-03-09 22:02:46 +0800

    更新部件布局 测试程序1

--- test/test_widget_1.c
+++ test/test_widget_1.c
@@ -7,66 +7,71 @@
 #include LC_WINDOW_H
 #include LC_LABEL_H
 #include LC_GRAPH_H
-#include <unistd.h>
-static void *thread(void *arg)
+
+static void thread(void *arg)
 {
 	LCUI_Widget *widget, *label;
+	
 	widget = (LCUI_Widget*)arg; 
-	label = Create_Widget( "label" );
+	label = Widget_New( "label" );
 	Widget_Container_Add( widget, label );
-	Set_Widget_Align( label, ALIGN_MIDDLE_CENTER, Pos(0,0) );
-	Show_Widget( label );
+	Widget_SetAlign( label, ALIGN_MIDDLE_CENTER, Pos(0,0) );
+	Widget_Show( label );
 	while(LCUI_Active()) {
 		Label_Text( label, "DOCK_TYPE_TOP" );
-		Set_Widget_Dock( widget, DOCK_TYPE_TOP ); 
-		Set_Widget_Size( widget, "120px", "120px" ); 
+		Widget_SetDock( widget, DOCK_TYPE_TOP ); 
+		Widget_SetSize( widget, "120px", "120px" ); 
 		/* 还原尺寸 */
-		sleep(1);
+		LCUI_Sleep(1);
 		Label_Text( label, "DOCK_TYPE_LEFT" );
-		Set_Widget_Dock( widget, DOCK_TYPE_LEFT ); 
-		Set_Widget_Size( widget, "120px", "120px" ); 
-		sleep(1);
+		Widget_SetDock( widget, DOCK_TYPE_LEFT ); 
+		Widget_SetSize( widget, "120px", "120px" ); 
+		LCUI_Sleep(1);
 		Label_Text( label, "DOCK_TYPE_RIGHT" );
-		Set_Widget_Dock( widget, DOCK_TYPE_RIGHT ); 
-		Set_Widget_Size( widget, "120px", "120px" ); 
-		sleep(1);
+		Widget_SetDock( widget, DOCK_TYPE_RIGHT ); 
+		Widget_SetSize( widget, "120px", "120px" ); 
+		LCUI_Sleep(1);
 		Label_Text( label, "DOCK_TYPE_BOTTOM" );
-		Set_Widget_Dock( widget, DOCK_TYPE_BOTTOM ); 
-		Set_Widget_Size( widget, "120px", "120px" ); 
-		sleep(1);
+		Widget_SetDock( widget, DOCK_TYPE_BOTTOM ); 
+		Widget_SetSize( widget, "120px", "120px" ); 
+		LCUI_Sleep(1);
 		Label_Text( label, "DOCK_TYPE_FILL" );
-		Set_Widget_Dock( widget, DOCK_TYPE_FILL ); 
-		Set_Widget_Size( widget, "120px", "120px" ); 
-		sleep(1);
+		Widget_SetDock( widget, DOCK_TYPE_FILL ); 
+		Widget_SetSize( widget, "120px", "120px" ); 
+		LCUI_Sleep(1);
 		Label_Text( label, "DOCK_TYPE_NONE" );
-		Set_Widget_Dock( widget, DOCK_TYPE_NONE ); 
-		Set_Widget_Size( widget, "120px", "120px" );  
-		sleep(1);
+		Widget_SetDock( widget, DOCK_TYPE_NONE ); 
+		Widget_SetSize( widget, "120px", "120px" );  
+		LCUI_Sleep(1);
 	}
-	LCUI_Thread_Exit(NULL);
+	LCUIThread_Exit(NULL);
 }
 
-int main(int argc, char *argv[])
+static void destroy( LCUI_Widget *widget, LCUI_WidgetEvent *unused )
 {
-	thread_t t;
-	LCUI_Init( argc, argv );
-	
+	LCUI_MainLoop_Quit(NULL);
+}
+
+int main(void)
+{
+	LCUI_Thread t;
 	LCUI_Widget *window, *widget;
 	
-	window = Create_Widget( "window" );
-	widget = Create_Widget( NULL );
-	Set_Window_Title_Text( window, "测试部件布局-1" );
-	Window_Client_Area_Add( window, widget );
-	Resize_Widget( widget, Size(120, 120) );
-	Resize_Widget( window, Size(320, 240) );
-	Set_Widget_BG_Mode( widget, BG_MODE_FILL_BACKCOLOR );
-	Set_Widget_Border( widget, RGB(0,0,0), Border(1,1,1,1) );
-	Set_Widget_Backcolor( widget, RGB(255,0,0) );
+	LCUI_Init();
+	window = Widget_New( "window" );
+	widget = Widget_New( NULL );
+	Window_SetTitleText( window, "测试部件布局-1" );
+	Window_ClientArea_Add( window, widget );
+	Widget_Resize( widget, Size(120, 120) );
+	Widget_Resize( window, Size(320, 240) );
+	Widget_SetBackgroundTransparent( widget, FALSE );
+	Widget_SetBorder( widget, Border(1, BORDER_STYLE_SOLID, RGB(0,0,0)) );
+	Widget_SetBackgroundColor( widget, RGB(255,0,0) );
 	
-	Show_Widget( widget );
-	Show_Widget( window );
-	
-	LCUI_Thread_Create( &t, NULL, thread, widget );
+	Widget_Show( widget );
+	Widget_Show( window );
+	Widget_Event_Connect( Window_GetCloseButton(window), EVENT_CLICKED, destroy );
+	LCUIThread_Create( &t, thread, widget );
 	
 	return LCUI_Main();
 }
