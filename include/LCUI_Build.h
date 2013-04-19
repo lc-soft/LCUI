4a7a767a56ebf2a76a953791ba5db9d0e9de3c69
Liu Chao <lc-soft@live.cn>
2013-04-19 21:30:51 +0800

    修改main函数的参数列表

--- include/LCUI_Build.h
+++ include/LCUI_Build.h
@@ -1,42 +1,42 @@
 /* ****************************************************************************
  * LCUI_Build.h -- macro definition defines the location of some header files
- * 
+ *
  * Copyright (C) 2013 by
  * Liu Chao
- * 
+ *
  * This file is part of the LCUI project, and may only be used, modified, and
  * distributed under the terms of the GPLv2.
- * 
+ *
  * (GPLv2 is abbreviation of GNU General Public License Version 2)
- * 
+ *
  * By continuing to use, modify, or distribute this file you indicate that you
  * have read the license and understand and accept it fully.
- *  
- * The LCUI project is distributed in the hope that it will be useful, but 
- * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 
+ *
+ * The LCUI project is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
  * or FITNESS FOR A PARTICULAR PURPOSE. See the GPL v2 for more details.
- * 
- * You should have received a copy of the GPLv2 along with this file. It is 
+ *
+ * You should have received a copy of the GPLv2 along with this file. It is
  * usually in the LICENSE.TXT file, If not, see <http://www.gnu.org/licenses/>.
  * ****************************************************************************/
- 
+
 /* ****************************************************************************
  * LCUI_Build.h -- 定义了一些头文件的位置的宏定义
  *
- * 版权所有 (C) 2013 归属于 
+ * 版权所有 (C) 2013 归属于
  * 刘超
- * 
+ *
  * 这个文件是LCUI项目的一部分，并且只可以根据GPLv2许可协议来使用、更改和发布。
  *
  * (GPLv2 是 GNU通用公共许可证第二版 的英文缩写)
- * 
+ *
  * 继续使用、修改或发布本文件，表明您已经阅读并完全理解和接受这个许可协议。
- * 
+ *
  * LCUI 项目是基于使用目的而加以散布的，但不负任何担保责任，甚至没有适销性或特
  * 定用途的隐含担保，详情请参照GPLv2许可协议。
  *
  * 您应已收到附随于本文件的GPLv2许可协议的副本，它通常在LICENSE.TXT文件中，如果
- * 没有，请查看：<http://www.gnu.org/licenses/>. 
+ * 没有，请查看：<http://www.gnu.org/licenses/>.
  * ****************************************************************************/
 
 #ifndef __LCUI_BUILD_H__
@@ -50,12 +50,12 @@
    * compilers used '__export' and required this to be after
    * the type.
    */
-#  define LCUI_EXPORT(type) type __export 
+#  define LCUI_EXPORT(type) type __export
 #else /* newer compiler */
 #  ifdef LCUI_EXPORTS
-#    define LCUI_EXPORT(type) extern __declspec(dllexport) type 
+#    define LCUI_EXPORT(type) extern __declspec(dllexport) type
 #  else
-#    define LCUI_EXPORT(type) /*extern __declspec(dllimport)*/ type 
+#    define LCUI_EXPORT(type) /*extern __declspec(dllimport)*/ type
 #  endif
 #endif /* compiler */
 
@@ -97,15 +97,15 @@
 #define LC_DISPLAY_H	<LCUI/LCUI_Display.h>
 #define LC_WIDGET_H	<LCUI/LCUI_Widget.h>
 #define LC_WIDGET_HPP	<LCUI/LCUI_Widget.hpp>
-#define LC_QUEUE_H	<LCUI/LCUI_Queue.h> 
-#define LC_CURSOR_H	<LCUI/LCUI_Cursor.h> 
-#define LC_WORK_H	<LCUI/LCUI_Work.h> 
+#define LC_QUEUE_H	<LCUI/LCUI_Queue.h>
+#define LC_CURSOR_H	<LCUI/LCUI_Cursor.h>
+#define LC_WORK_H	<LCUI/LCUI_Work.h>
 #define LC_INPUT_H	<LCUI/LCUI_Input.h>
 #define LC_MEM_H	<LCUI/LCUI_Memory.h>
 #define LC_ERROR_H	<LCUI/LCUI_Error.h>
-#define LC_THREAD_H	<LCUI/LCUI_Thread.h> 
-#define LC_THREAD_HPP	<LCUI/LCUI_Thread.hpp> 
-#define LC_RES_H	<LCUI/LCUI_Resources.h> 
+#define LC_THREAD_H	<LCUI/LCUI_Thread.h>
+#define LC_THREAD_HPP	<LCUI/LCUI_Thread.hpp>
+#define LC_RES_H	<LCUI/LCUI_Resources.h>
 
 #define LC_GRAPHLAYER_H		<LCUI/LCUI_GraphLayer.h>
 
@@ -128,7 +128,7 @@
 #define LC_MENU_H	<LCUI/widget/menu.h>
 #define LC_CHECKBOX_H	<LCUI/widget/checkbox.h>
 #define LC_RADIOBTN_H	<LCUI/widget/radiobutton.h>
-#define LC_ACTIVEBOX_H	<LCUI/widget/activebox.h> 
+#define LC_ACTIVEBOX_H	<LCUI/widget/activebox.h>
 #define LC_TEXTBOX_H	<LCUI/widget/textbox.h>
 #define LC_SCROLLBAR_H	<LCUI/widget/scrollbar.h>
 
@@ -141,25 +141,25 @@
 #ifdef DEBUG
 #define DEBUG_MSG(format, ...) _DEBUG_MSG(format, ...)
 #else
-#define DEBUG_MSG(format, ...) 
+#define DEBUG_MSG(format, ...)
 #endif
 
 #ifdef DEBUG1
 #define DEBUG_MSG1(format, ...) _DEBUG_MSG(format, ...)
 #else
-#define DEBUG_MSG1(format, ...) 
+#define DEBUG_MSG1(format, ...)
 #endif
 
 #ifdef DEBUG2
 #define DEBUG_MSG2(format, ...) _DEBUG_MSG(format, ...)
 #else
-#define DEBUG_MSG2(format, ...) 
+#define DEBUG_MSG2(format, ...)
 #endif
 
 #ifdef DEBUG3
 #define DEBUG_MSG3(format, ...) _DEBUG_MSG(format, ...)
 #else
-#define DEBUG_MSG3(format, ...) 
+#define DEBUG_MSG3(format, ...)
 
 
 #if defined(WIN32) || defined(_WIN32)
@@ -197,7 +197,7 @@
 #define LCUI_DEFAULT_ARG	hInstance
 #define LCUI_DEFAULT_CONFIG	LCUI_INIT_MODE_AUTO,LCUI_DEFAULT_ARG
 #else
-#define LCUI_ARGLIST	 	int argc, char *argv
+#define LCUI_ARGLIST	 	int argc, char *argv[]
 #define	LCUIMainFunc		main
 #define LCUI_DEFAULT_ARG	NULL
 #define LCUI_DEFAULT_CONFIG	LCUI_INIT_MODE_AUTO,LCUI_DEFAULT_ARG
