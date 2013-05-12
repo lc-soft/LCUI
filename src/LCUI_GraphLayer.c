b9b55d5bf3c166c803f77c86348406cb16ffd6d2
Liu Chao <lc-soft@live.cn>
2013-05-12 15:58:53 +0800

    调整代码及注释的格式

--- src/LCUI_GraphLayer.c
+++ src/LCUI_GraphLayer.c
@@ -1,23 +1,23 @@
 /** ******************************************************************************
  * @file	LCUI_GraphLayer.c
  * @brief	GraphLayer operation set.
- * @author  Liu Chao <lc-soft@live.cn>
+ * @author	Liu Chao <lc-soft@live.cn>
  * @warning
- * Copyright (C) 2012-2013 by													\n
- * Liu Chao																		\n
- * 																				\n
+ * Copyright (C) 2012-2013 by							\n
+ * Liu Chao									\n
+ * 										\n
  * This file is part of the LCUI project, and may only be used, modified, and	\n
- * distributed under the terms of the GPLv2.									\n
- * 																				\n
- * (GPLv2 is abbreviation of GNU General Public License Version 2)				\n
- * 																				\n
+ * distributed under the terms of the GPLv2.					\n
+ * 										\n
+ * (GPLv2 is abbreviation of GNU General Public License Version 2)		\n
+ * 										\n
  * By continuing to use, modify, or distribute this file you indicate that you	\n
- * have read the license and understand and accept it fully.					\n
- *  																			\n
- * The LCUI project is distributed in the hope that it will be useful, but 		\n
+ * have read the license and understand and accept it fully.			\n
+ *  										\n
+ * The LCUI project is distributed in the hope that it will be useful, but 	\n
  * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 	\n
- * or FITNESS FOR A PARTICULAR PURPOSE. See the GPL v2 for more details.		\n
- * 																				\n
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GPL v2 for more details.	\n
+ * 										\n
  * You should have received a copy of the GPLv2 along with this file. It is 	\n
  * usually in the LICENSE.TXT file, If not, see <http://www.gnu.org/licenses/>.	\n
  * ******************************************************************************/
@@ -25,22 +25,22 @@
 /** ******************************************************************************
  * @file	LCUI_GraphLayer.c
  * @brief	图层的操作集.
- * @author  刘超 <lc-soft@live.cn>
+ * @author	刘超 <lc-soft@live.cn>
  * @warning
- * 版权所有 (C) 2012-2013 归属于												\n
- * 刘超																			\n
- * 																				\n
+ * 版权所有 (C) 2012-2013 归属于							\n
+ * 刘超										\n
+ * 										\n
  * 这个文件是LCUI项目的一部分，并且只可以根据GPLv2许可协议来使用、更改和发布。	\n
- * 																				\n
- * (GPLv2 是 GNU通用公共许可证第二版 的英文缩写)								\n
- * 																				\n
+ * 										\n
+ * (GPLv2 是 GNU通用公共许可证第二版 的英文缩写)					\n
+ * 										\n
  * 继续使用、修改或发布本文件，表明您已经阅读并完全理解和接受这个许可协议。		\n
- * 																				\n
- * LCUI 项目是基于使用目的而加以散布的，但不负任何担保责任，甚至没有适销性或特定\n
- * 用途的隐含担保，详情请参照GPLv2许可协议。									\n
- * 																				\n
+ * 										\n
+ * LCUI 项目是基于使用目的而加以散布的，但不负任何担保责任，甚至没有适销性或特定用途	\n
+ * 的隐含担保，详情请参照GPLv2许可协议。						\n
+ * 										\n
  * 您应已收到附随于本文件的GPLv2许可协议的副本，它通常在LICENSE.TXT文件中，如果	\n
- * 没有，请查看：<http://www.gnu.org/licenses/>. 								\n
+ * 没有，请查看：<http://www.gnu.org/licenses/>. 				\n
  * ******************************************************************************/
 #include <LCUI_Build.h>
 #include LC_LCUI_H
@@ -128,7 +128,7 @@ LCUI_API LCUI_GraphLayer* GraphLayer_New( void )
 
 
 LCUI_API int GraphLayer_AddChild(	LCUI_GraphLayer *des_ctnr,
-									LCUI_GraphLayer *glayer )
+					LCUI_GraphLayer *glayer )
 {
 	int i, total;
 	LCUI_GraphLayer *tmp_child;
@@ -163,7 +163,7 @@ LCUI_API int GraphLayer_AddChild(	LCUI_GraphLayer *des_ctnr,
 }
 
 LCUI_API int GraphLayer_MoveChild(	LCUI_GraphLayer *new_ctnr, 
-									LCUI_GraphLayer *glayer )
+					LCUI_GraphLayer *glayer )
 {
 	int ret;
 	//_DEBUG_MSG( "new_ctnr: %p, glayer: %p\n", new_ctnr, glayer );
@@ -293,7 +293,7 @@ LCUI_API LCUI_Graph* GraphLayer_GetSelfGraph( LCUI_GraphLayer *glayer )
 }
 
 LCUI_API LCUI_Rect GraphLayer_GetValidRect(	LCUI_GraphLayer *root_glayer,
-											LCUI_GraphLayer *glayer )
+						LCUI_GraphLayer *glayer )
 {
 	int temp; 
 	LCUI_Pos pos;
@@ -375,7 +375,7 @@ LCUI_API LCUI_Rect GraphLayer_GetValidRect(	LCUI_GraphLayer *root_glayer,
 
 LCUI_API LCUI_Pos
 GraphLayer_GetGlobalPos(	LCUI_GraphLayer *root_glayer,
-							LCUI_GraphLayer *glayer )
+				LCUI_GraphLayer *glayer )
 {
 	LCUI_Pos pos;
 	if( !glayer || !root_glayer || glayer == root_glayer ) {
@@ -439,8 +439,8 @@ __GraphLayer_GetLayers(
 }
 
 LCUI_API int GraphLayer_GetLayers(	LCUI_GraphLayer *glayer, 
-									LCUI_Rect rect,
-									LCUI_Queue *queue )
+					LCUI_Rect rect,
+					LCUI_Queue *queue )
 {
 	//_DEBUG_MSG("rect: %d,%d,%d,%d\n", 
 	//rect.x, rect.y, rect.width, rect.height);
@@ -462,8 +462,8 @@ static uchar_t GraphLayer_GetRealAlpha( LCUI_GraphLayer *glayer )
 
 
 LCUI_API int GraphLayer_GetGraph(	LCUI_GraphLayer *ctnr, 
-									LCUI_Graph *graph_buff,
-									LCUI_Rect rect )
+					LCUI_Graph *graph_buff,
+					LCUI_Rect rect )
 {
 	static int i, total; 
 	static uchar_t tmp_alpha, alpha;
