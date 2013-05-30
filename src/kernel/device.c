f8e70254ec1c78e71e41298427ddee257de1b553
Liu Chao <lc-soft@live.cn>
2013-05-30 20:34:09 +0800

    私有化设备列表

--- src/kernel/device.c
+++ src/kernel/device.c
@@ -41,6 +41,8 @@
 #include <LCUI_Build.h>
 #include LC_LCUI_H
 
+static LCUI_Queue dev_list; /* 设备列表 */
+
 /*----------------------------- Device -------------------------------*/
 /* 设备列表初始化 */
 static void 
@@ -66,7 +68,7 @@ LCUIDevice_Add(	LCUI_BOOL (*init_func)(void),
 	data.init_func = init_func;
 	data.proc_func = proc_func;
 	data.destroy_func = destroy_func;
-	if( 0<= Queue_Add( &LCUI_Sys.dev_list, &data ) ) {
+	if( 0 <= Queue_Add( &dev_list, &data ) ) {
 		return 0;
 	}
 	return -1;
@@ -109,9 +111,9 @@ proc_dev_list ( void *arg )
 LCUI_API int
 LCUIModule_Device_Init(void)
 {
-	dev_list_init( &LCUI_Sys.dev_list );
+	dev_list_init( &dev_list );
 	return _LCUIThread_Create( &LCUI_Sys.dev_thread,
-			proc_dev_list, &LCUI_Sys.dev_list );
+			proc_dev_list, &dev_list );
 }
 
 /* 停用设备处理模块 */
@@ -120,12 +122,10 @@ LCUIModule_Device_End(void)
 {
 	int total, i;
 	dev_func_data *data_ptr;
-	LCUI_Queue *dev_list;
 	
-	dev_list = &LCUI_Sys.dev_list; 
-	total = Queue_GetTotal( dev_list );
+	total = Queue_GetTotal( &dev_list );
 	for(i=0; i<total; ++i) {
-		data_ptr = Queue_Get( dev_list, i );
+		data_ptr = (dev_func_data*)Queue_Get( &dev_list, i );
 		if( !data_ptr || !data_ptr->destroy_func ) {
 			continue;
 		}
