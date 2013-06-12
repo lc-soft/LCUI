10ff8c284e629ffa211a5575485767e289bdd4e4
Liu Chao <lc-soft@live.cn>
2013-06-12 15:25:21 +0800

    改写滑动解锁程序，用定时器实现内容更新

--- test/screenlock/screenlock.c
+++ test/screenlock/screenlock.c
@@ -1,4 +1,4 @@
-// 一个屏幕滑动解锁的程序
+﻿// 一个屏幕滑动解锁的程序
 #define I_NEED_WINMAIN
 #include <LCUI_Build.h>
 #include LC_LCUI_H
@@ -8,11 +8,7 @@
 #include LC_MISC_H
 #include LC_GRAPH_H
 #include LC_PICBOX_H
-#include <unistd.h>
-#include <pthread.h>
 #include <time.h>
-
-#define PATH_FONTFILE "../../fonts/msyh.ttf"
 
 /* 移动滑块 */
 void move_pic_btn(LCUI_Widget *widget, LCUI_WidgetEvent *event)
@@ -38,82 +34,134 @@ void move_pic_btn(LCUI_Widget *widget, LCUI_WidgetEvent *event)
 	}
 }
 
+static LCUI_Widget	*pic_bg, *time_box, *pic_btn_line, *pic_btn,
+			*win, *pic_l1, *pic_l2,
+			*pic_c, *pic_r1, *pic_r2;
+
+static LCUI_Widget *wday_label, *date_label;
+static LCUI_Graph img_digital[9], img_dot, img_bg, img_btn, img_btn_bg;
+
+static void LoadIMG(void)
+{
+	int i;
+	char filepath[1024];
+	for(i=0; i<10; ++i) {
+		sprintf( filepath, "drawable/time_%d.png", i );
+		Graph_Init( &img_digital[i] );
+		Graph_LoadImage( filepath, &img_digital[i] );
+	}
+	Graph_Init( &img_dot );
+	Graph_Init( &img_btn );
+	Graph_Init( &img_btn_bg );
+	Graph_Init( &img_bg );
+	Graph_LoadImage( "drawable/time_dot.png", &img_dot );
+	Graph_LoadImage( "drawable/btn.png", &img_btn );
+	Graph_LoadImage( "drawable/btn_bg.png", &img_btn_bg );
+	Graph_LoadImage( "drawable/bg.png", &img_bg );
+}
+
+static void FreeIMG(void)
+{
+	int i;
+	for(i=0; i<10; ++i) {
+		Graph_Free( &img_digital[i] );
+	}
+	Graph_Free( &img_dot );
+	Graph_Free( &img_btn );
+	Graph_Free( &img_btn_bg );
+	Graph_Free( &img_bg );
+}
+
 /* 动态改变label部件的文本内容 */
-void update_time(void *arg)
+static void update_time(void)
 {
 	time_t rawtime;
-	struct tm * timeinfo;
-	char filename[100], str[256];
-	LCUI_Widget *wday_label, *date_label;
-	int l1=0,l2=0,r1=0,r2=0,old_l1=0, old_l2=0, old_r1=0,old_r2=0;
-	char day[][20] = {"星期天","星期一","星期二","星期三","星期四","星期五","星期六"};
-	LCUI_Widget	*pic_bg, *time_box, *pic_btn_line, *pic_btn,
-			*win, *pic_l1, *pic_l2,
-			 *pic_c, *pic_r1, *pic_r2;
+	struct tm *timeinfo;
+	wchar_t date_text[64];
+	wchar_t day[][20] = {
+		L"星期天", L"星期一", L"星期二", 
+		L"星期三", L"星期四", L"星期五", L"星期六"};
 
-	win = (LCUI_Widget *)arg; /* 转换类型 */
+	time ( &rawtime );
+	/* 获取系统当前时间 */
+	timeinfo = localtime ( &rawtime ); 
+	swprintf( date_text, 64,
+		L"<color=40,165,45>%4d年%02d月%02d日</color>",
+		timeinfo->tm_year+1900, timeinfo->tm_mon+1,
+		timeinfo->tm_mday );
+
+	Label_TextW( date_label, date_text );
+
+	swprintf( date_text, 64,
+		L"<color=40,165,45>%s</color>",
+		day[(int) (timeinfo->tm_wday)] );
+
+	Label_TextW( wday_label, date_text );
+
+	Widget_SetBackgroundImage( pic_l1, &img_digital[timeinfo->tm_hour/10] );
+	Widget_SetBackgroundImage( pic_l2, &img_digital[timeinfo->tm_hour%10] );
+	Widget_SetBackgroundImage( pic_r1, &img_digital[timeinfo->tm_min/10] );
+	Widget_SetBackgroundImage( pic_r2, &img_digital[timeinfo->tm_min%10] );
+}
+
+static void CreateGUI( LCUI_Widget *win )
+{
 	/* 创建部件 */
 	time_box	= Widget_New(NULL);
-	pic_bg		= Widget_New("picture_box");
-	pic_l1		= Widget_New("picture_box");
-	pic_l2		= Widget_New("picture_box");
-	pic_c		= Widget_New("picture_box");
-	pic_btn		= Widget_New("picture_box");
-	pic_btn_line	= Widget_New("picture_box");
-	pic_btn		= Widget_New("picture_box");
-	pic_r1		= Widget_New("picture_box");
-	pic_r2		= Widget_New("picture_box");
+	pic_bg		= Widget_New(NULL);
+	pic_l1		= Widget_New(NULL);
+	pic_l2		= Widget_New(NULL);
+	pic_c		= Widget_New(NULL);
+	pic_btn		= Widget_New(NULL);
+	pic_btn_line	= Widget_New(NULL);
+	pic_btn		= Widget_New(NULL);
+	pic_r1		= Widget_New(NULL);
+	pic_r2		= Widget_New(NULL);
 	date_label	= Widget_New("label");
 	wday_label	= Widget_New("label");
-
-	/* 生成文件路径 */
-	strcpy(filename, "drawable/time_0.png");
-	/* 载入图片 */
-	PictureBox_SetImageFile(pic_bg, "drawable/bg.png");
-	PictureBox_SetImageFile(pic_btn_line, "drawable/btn_bg.png");
-	PictureBox_SetImageFile(pic_btn, "drawable/btn.png");
-	PictureBox_SetImageFile(pic_l1, filename);
-	PictureBox_SetImageFile(pic_l2, filename);
-	PictureBox_SetImageFile(pic_c,  "drawable/time_dot.png");
-	PictureBox_SetImageFile(pic_r1, filename);
-	PictureBox_SetImageFile(pic_r2, filename);
-
-	PictureBox_SetSizeMode(pic_btn_line, SIZE_MODE_CENTER);
-
+	/* 设置这些部件的初始背景图 */
+	Widget_SetBackgroundImage( pic_bg, &img_bg );
+	Widget_SetBackgroundImage( pic_btn_line, &img_btn_bg );
+	Widget_SetBackgroundImage( pic_btn, &img_btn );
+	Widget_SetBackgroundImage( pic_l1, &img_digital[0] );
+	Widget_SetBackgroundImage( pic_l2, &img_digital[0] );
+	Widget_SetBackgroundImage( pic_r1, &img_digital[0] );
+	Widget_SetBackgroundImage( pic_r2, &img_digital[0] );
+	Widget_SetBackgroundImage( pic_c,  &img_dot );
 	/* 调整尺寸 */
-	Widget_Resize(time_box,		Size(162, 38));
-	Widget_Resize(pic_bg,		Size(300, 90));
-	Widget_Resize(pic_btn_line,	Size(245, 66));
-	Widget_Resize(pic_btn,		Size(46, 66));
-	Widget_Resize(pic_l1,		Size(33, 36));
-	Widget_Resize(pic_l2,		Size(33, 36));
-	Widget_Resize(pic_c,		Size(30, 38));
-	Widget_Resize(pic_r1,		Size(33, 36));
-	Widget_Resize(pic_r2,		Size(33, 36));
+	Widget_Resize( time_box,	Size(162, 38) );
+	Widget_Resize( pic_bg,		Size(300, 90) );
+	Widget_Resize( pic_btn_line,	Size(245, 66) );
+	Widget_Resize( pic_btn,		Size(46, 66) );
+	Widget_Resize( pic_l1,		Size(33, 36) );
+	Widget_Resize( pic_l2,		Size(33, 36) );
+	Widget_Resize( pic_c,		Size(30, 38) );
+	Widget_Resize( pic_r1,		Size(33, 36) );
+	Widget_Resize( pic_r2,		Size(33, 36) );
 	/* 调整布局 */
-	Widget_SetAlign(pic_l1, ALIGN_MIDDLE_CENTER, Pos(-66,0));
-	Widget_SetAlign(pic_l2, ALIGN_MIDDLE_CENTER, Pos(-33,0));
-	Widget_SetAlign(pic_c, ALIGN_MIDDLE_CENTER, Pos(0,0));
-	Widget_SetAlign(pic_r1, ALIGN_MIDDLE_CENTER, Pos(33,0));
-	Widget_SetAlign(pic_r2, ALIGN_MIDDLE_CENTER, Pos(66,0));
-	Widget_SetAlign(time_box, ALIGN_MIDDLE_CENTER, Pos(0,-50));
-	Widget_SetAlign(pic_bg, ALIGN_MIDDLE_CENTER, Pos(0,5));
-	Widget_SetAlign(date_label, ALIGN_MIDDLE_CENTER, Pos(0,35-50));
-	Widget_SetAlign(wday_label, ALIGN_TOP_CENTER, Pos(0,5));
-	Widget_SetAlign(pic_btn_line, ALIGN_BOTTOM_CENTER, Pos(0,-10));
+	Widget_SetAlign( pic_l1, ALIGN_MIDDLE_CENTER, Pos(-66,0) );
+	Widget_SetAlign( pic_l2, ALIGN_MIDDLE_CENTER, Pos(-33,0) );
+	Widget_SetAlign( pic_c, ALIGN_MIDDLE_CENTER, Pos(0,0) );
+	Widget_SetAlign( pic_r1, ALIGN_MIDDLE_CENTER, Pos(33,0) );
+	Widget_SetAlign( pic_r2, ALIGN_MIDDLE_CENTER, Pos(66,0) );
+	Widget_SetAlign( time_box, ALIGN_MIDDLE_CENTER, Pos(0,-50) );
+	Widget_SetAlign( pic_bg, ALIGN_MIDDLE_CENTER, Pos(0,5) );
+	Widget_SetAlign(date_label, ALIGN_MIDDLE_CENTER, Pos(0,35-50) );
+	Widget_SetAlign( wday_label, ALIGN_TOP_CENTER, Pos(0,5) );
+	Widget_SetAlign( pic_btn_line, ALIGN_BOTTOM_CENTER, Pos(0,-10) );
 	/* 放入容器 */
-	Widget_Container_Add(time_box, pic_l1);
-	Widget_Container_Add(time_box, pic_l2);
-	Widget_Container_Add(time_box, pic_c);
-	Widget_Container_Add(time_box, pic_r1);
-	Widget_Container_Add(time_box, pic_r2);
-	Widget_Container_Add(pic_btn_line, pic_btn);
+	Widget_Container_Add( time_box, pic_l1 );
+	Widget_Container_Add( time_box, pic_l2 );
+	Widget_Container_Add( time_box, pic_c );
+	Widget_Container_Add( time_box, pic_r1 );
+	Widget_Container_Add( time_box, pic_r2 );
+	Widget_Container_Add( pic_btn_line, pic_btn );
 
-	Window_ClientArea_Add(win, pic_bg);
-	Window_ClientArea_Add(win, time_box);
-	Window_ClientArea_Add(win, date_label);
-	Window_ClientArea_Add(win, wday_label);
-	Window_ClientArea_Add(win, pic_btn_line);
+	Window_ClientArea_Add( win, pic_bg );
+	Window_ClientArea_Add( win, time_box );
+	Window_ClientArea_Add( win, date_label );
+	Window_ClientArea_Add( win, wday_label );
+	Window_ClientArea_Add (win, pic_btn_line );
 	/* 限制移动范围 */
 	Widget_LimitPos(pic_btn, Pos(0,0), Pos(195,0));
 	Widget_Event_Connect(pic_btn, EVENT_DRAG, move_pic_btn);
@@ -130,59 +178,33 @@ void update_time(void *arg)
 	Widget_Show(pic_btn);
 	Widget_Show(pic_btn_line);
 
-	while(1) {
-		time ( &rawtime );
-		timeinfo = localtime ( &rawtime ); /* 获取系统当前时间 */
-		sprintf( str, "<color=40,165,45>%4d年%02d月%02d日</color>",
-			timeinfo->tm_year+1900, timeinfo->tm_mon+1,
-			timeinfo->tm_mday );
-		Label_Text( date_label, str );
-		sprintf( str, "<color=40,165,45>%s</color>",
-			day[(int) (timeinfo->tm_wday)] );
-		Label_Text( wday_label, str );
-		l1 = timeinfo->tm_hour/10;
-		l2 = timeinfo->tm_hour%10;
-		r1 = timeinfo->tm_min/10;
-		r2 = timeinfo->tm_min%10;
-		if(l1 != old_l1) {
-			sprintf(filename, "drawable/time_%d.png", l1);
-			PictureBox_SetImageFile(pic_l1, filename);
-			old_l1 = l1;
-		}
-		if(l2 != old_l2) {
-			sprintf(filename, "drawable/time_%d.png", l2);
-			PictureBox_SetImageFile(pic_l2, filename);
-			old_l2 = l2;
-		}
-		if(l1 != old_r1) {
-			sprintf(filename, "drawable/time_%d.png", r1);
-			PictureBox_SetImageFile(pic_r1, filename);
-			old_r1 = r1;
-		}
-		if(r2 != old_r2) {
-			sprintf(filename, "drawable/time_%d.png", r2);
-			PictureBox_SetImageFile(pic_r2, filename);
-			old_r2 = r2;
-		}
-		sleep(1);/* 暂停1秒 */
-	}
-	LCUIThread_Exit(NULL);
+	update_time();
 }
 
 int main( int argc, char **argv )
 {
 	LCUI_Thread t;
 	LCUI_Widget *window;
-
-	LCUI_Init(320,240,0);
+	/* 载入图像资源 */
+	LoadIMG();
+	/* 初始化LCUI */
+	LCUI_Init(350,280,0);
+	/* 创建窗口 */
 	window = Widget_New("window");
-	Widget_SetBackgroundColor(window, RGB(255,255,255));
-	Widget_Resize(window, Size(320, 240));
+	/* 设置窗口背景色 */
+	Widget_SetBackgroundColor( window, RGB(255,255,255) );
+	/* 调整窗口尺寸 */
+	Widget_Resize( window, Size(320, 240) );
 	/* 设置窗口为线条边框风格 */
-	Widget_SetStyleID( window, WINDOW_STYLE_LINE );
-	/* 创建线程，用于更新时间显示 */
-	LCUIThread_Create(&t, update_time, (void*)window);
+	Widget_SetStyleID( window, WINDOW_STYLE_NONE );
+	/* 将界面创建至窗口内 */
+	CreateGUI( window );
+	/* 设置定时器，用于定时更新时间显示 */
+	LCUITimer_Set( 1000, update_time, TRUE );
+	/* 显示窗口 */
 	Widget_Show( window );
+	/* 在LCUI退出时释放图像资源 */
+	LCUIApp_AtQuit( FreeIMG );
 	return LCUI_Main();
 }
 
