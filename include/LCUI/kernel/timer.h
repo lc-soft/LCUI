1a4fb79d11bd72c070f3841d26af1e4320704637
Liu Chao <lc-soft@live.cn>
2013-07-10 20:20:02 +0800

    更新头文件

--- include/LCUI/kernel/timer.h
+++ include/LCUI/kernel/timer.h
@@ -43,44 +43,69 @@
 
 LCUI_BEGIN_HEADER
 
-/* 
- * 功能：设置定时器，在指定的时间后调用指定回调函数 
- * 说明：时间单位为毫秒，调用后会返回该定时器的标识符; 
- * 如果要用于循环定时处理某些任务，可将 reuse 置为 1，否则置于 0。
+/** 
+ * 设置定时器
+ * 定时器的作用是让一个任务在经过指定时间后才执行
+ * @param n_ms
+ *	等待的时间，单位为毫秒
+ * @param callback_func
+ *	用于响应定时器的回调函数
+ * @param reuse 
+ *	指示该定时器是否重复使用，如果要用于循环定时处理某些任
+ *	务，可将它置为 TRUE，否则置于 FALSE。
+ * @return 
+ *	该定时器的标识符
  * */
-LCUI_API int 
-LCUITimer_Set( long int n_ms, void (*callback_func)(void), LCUI_BOOL reuse );
+LCUI_API int LCUITimer_Set(	long int n_ms,
+				void (*callback_func)(void*),
+				void *arg,
+				LCUI_BOOL reuse );
 
-/*
- * 功能：释放定时器
- * 说明：当不需要定时器时，可以使用该函数释放定时器占用的资源
- * 返回值：正常返回0，指定ID的定时器不存在则返回-1.
+/**
+ * 释放定时器
+ * 当不需要定时器时，可以使用该函数释放定时器占用的资源
+ * @param timer_id
+ *	需要释放的定时器的标识符
+ * @return
+ *	正常返回0，指定ID的定时器不存在则返回-1.
  * */
-LCUI_API int
-LCUITimer_Free( int timer_id );
+LCUI_API int LCUITimer_Free( int timer_id );
 
-/*
- * 功能：暂停定时器的使用 
- * 说明：一般用于往复定时的定时器
+/**
+ * 暂停定时器的倒计时
+ * 一般用于往复定时的定时器
+ * @param timer_id
+ *	目标定时器的标识符
+ * @return
+ *	正常返回0，指定ID的定时器不存在则返回-1.
  * */
-LCUI_API int
-LCUITimer_Pause( int timer_id );
+LCUI_API int LCUITimer_Pause( int timer_id );
 
-/* 继续使用定时器 */
-LCUI_API int
-LCUITimer_Continue( int timer_id );
+/**
+ * 继续定时器的倒计时
+ * @param timer_id
+ *	目标定时器的标识符
+ * @return
+ *	正常返回0，指定ID的定时器不存在则返回-1.
+ * */
+LCUI_API int LCUITimer_Continue( int timer_id );
 
-/* 重设定时器的时间 */
-LCUI_API int
-LCUITimer_Reset( int timer_id, long int n_ms );
+/**
+ * 重设定时器的等待时间
+ * @param timer_id
+ *	需要释放的定时器的标识符
+ * @param n_ms
+ *	等待的时间，单位为毫秒
+ * @return
+ *	正常返回0，指定ID的定时器不存在则返回-1.
+ * */
+LCUI_API int LCUITimer_Reset( int timer_id, long int n_ms ) ;
 
 /* 初始化定时器模块 */
-LCUI_API void
-LCUIModule_Timer_Init( void );
+LCUI_API void LCUIModule_Timer_Init( void );
 
 /* 停用定时器模块 */
-LCUI_API void
-LCUIModule_Timer_End( void );
+LCUI_API void LCUIModule_Timer_End( void );
 
 LCUI_END_HEADER
 
