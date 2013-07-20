c7c74e69ea43aa3b724454e9150c1e228d3531f1
Liu Chao <lc-soft@live.cn>
2013-07-20 20:31:06 +0800

    更新函数声明

--- include/LCUI/LCUI_Display.h
+++ include/LCUI/LCUI_Display.h
@@ -63,104 +63,82 @@ LCUI_API HWND Win32_GetSelfHWND( void );
 LCUI_API void Win32_SetSelfHWND( HWND hwnd );
 #endif
 
-/* 
- * 功能：在屏幕上指定位置放置图形
- * 说明：此函数使用帧缓冲（FrameBuffer）进行图形输出
- * *注：主要代码参考自mgaveiw的mga_vfb.c文件中的write_to_fb函数.
- * */
-LCUI_API int
-LCUIScreen_PutGraph (LCUI_Graph *src, LCUI_Pos pos );
-
-/* 
- * 功能：直接读取帧缓冲中的图像数据
- * 说明：效率较高，但捕获的图像有可能会有问题。
- * */
-LCUI_API void
-LCUIScreen_CatchGraph( LCUI_Rect area, LCUI_Graph *out );
-
-/* 将帧缓冲的图像同步显示至屏幕 */
-LCUI_API void
-LCUIScreen_SyncFrameBuffer( void );
-
-/*
- * 功能：获取屏幕宽度
- * 返回值：屏幕的宽度，单位为像素，必须在使用LCUI_Init()函数后使用，否则无效
- * */
-LCUI_API int
-LCUIScreen_GetWidth( void );
-
-/*
- * 功能：获取屏幕高度
- * 返回值：屏幕的高度，单位为像素，必须在使用LCUI_Init()函数后使用，否则无效
- * */
-LCUI_API int
-LCUIScreen_GetHeight( void );
-
-/* 获取屏幕尺寸 */
-LCUI_API LCUI_Size
-LCUIScreen_GetSize( void );
-
-/* 填充指定位置的像素点的颜色 */
 LCUI_API void
 LCUIScreen_FillPixel( LCUI_Pos pos, LCUI_RGB color );
 
-/* 获取屏幕内显示的图像 */
 LCUI_API int
 LCUIScreen_GetGraph( LCUI_Graph *out );
 
-/* 设置屏幕内的指定区域为无效区域，以便刷新该区域内的图形显示 */
 LCUI_API int
-LCUIScreen_InvalidArea( LCUI_Rect rect );
+LCUIScreen_Init( int w, int h, int mode );
 
-/* 功能：获取屏幕中的每个像素的表示所用的位数 */
+/* 设置视频输出模式 */
 LCUI_API int
-LCUIScreen_GetBits( void );
+LCUIScreen_SetMode( int w, int h, int mode );
 
-/* 获取屏幕显示模式 */
 LCUI_API int
-LCUIScreen_GetMode( void );
+LCUIScreen_Destroy( void );
 
-/* 获取屏幕信息 */
+LCUI_API void
+LCUIScreen_SyncFrameBuffer( void );
+
+LCUI_API int
+LCUIScreen_PutGraph (LCUI_Graph *graph, LCUI_Pos des_pos );
+
+LCUI_API void
+LCUIScreen_CatchGraph( LCUI_Rect area, LCUI_Graph *out );
+
+
+/** 获取屏幕宽度 */
+LCUI_API int LCUIScreen_GetWidth( void );
+
+/** 获取屏幕高度 */
+LCUI_API int LCUIScreen_GetHeight( void );
+
+/** 获取屏幕尺寸 */
+LCUI_API LCUI_Size LCUIScreen_GetSize( void );
+
+/** 获取屏幕无效区域队列的指针 */
+LCUI_API LCUI_RectQueue* LCUIScreen_GetInvalidAreaQueue( void );
+
+/** 设置屏幕内的指定区域为无效区域 */
+LCUI_API int LCUIScreen_InvalidArea( LCUI_Rect rect );
+
+/** 获取屏幕每个像素点的色彩值所占的位数 */
+LCUI_API int LCUIScreen_GetBits( void );
+
+/** 获取屏幕显示模式 */
+LCUI_API int LCUIScreen_GetMode( void );
+
+/** 获取屏幕信息 */
 LCUI_API void LCUIScreen_GetInfo( LCUI_Screen *info );
 
-/* 设置屏幕信息 */
+/** 设置屏幕信息 */
 LCUI_API void LCUIScreen_SetInfo( LCUI_Screen *info );
 
-/* 获取屏幕中心点的坐标 */
-LCUI_API LCUI_Pos
-LCUIScreen_GetCenter( void );
+/** 获取屏幕中心点的坐标 */
+LCUI_API LCUI_Pos LCUIScreen_GetCenter( void );
 
-/* 为图层树锁上互斥锁 */
-LCUI_API void 
-LCUIScreen_LockGraphLayerTree( void );
+/** 为图层树锁上互斥锁 */
+LCUI_API void LCUIScreen_LockGraphLayerTree( void );
 
-/* 解除图层树互斥锁 */
-LCUI_API void 
-LCUIScreen_UnlockGraphLayerTree( void );
+/** 解除图层树互斥锁 */
+LCUI_API void LCUIScreen_UnlockGraphLayerTree( void );
 
-/* 获取屏幕中指定区域内实际要显示的图形 */
-LCUI_API void
-LCUIScreen_GetRealGraph( LCUI_Rect rect, LCUI_Graph *graph );
+/** 获取屏幕中指定区域内实际要显示的图形 */
+LCUI_API void LCUIScreen_GetRealGraph( LCUI_Rect rect, LCUI_Graph *graph );
 
-/* 标记需要同步无效区域 */
+/** 标记需要同步无效区域 */
 LCUI_API void LCUIScreen_MarkSync(void);
 
-/* 设置模式 */
-LCUI_API int LCUIScreen_SetMode( int w, int h, int mode );
+/** 获取当前的屏幕内容每秒更新的帧数 */
+LCUI_API int LCUIScreen_GetFPS(void);
 
-/* 初始化屏幕 */
-LCUI_API int LCUIScreen_Init( int w, int h, int mode );
+/** 初始化图形输出模块 */
+LCUI_API int LCUIModule_Video_Init( int w, int h, int mode );
 
-/* 销毁屏幕 */
-LCUI_API int LCUIScreen_Destroy( void );
-
-/* 初始化图形输出模块 */
-LCUI_API int
-LCUIModule_Video_Init( int w, int h, int mode );
-
-/* 停用图形输出模块 */
-LCUI_API int
-LCUIModule_Video_End( void );
+/** 停用图形输出模块 */
+LCUI_API int LCUIModule_Video_End( void );
 
 LCUI_END_HEADER
 
