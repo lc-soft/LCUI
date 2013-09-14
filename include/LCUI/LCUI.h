3700597fc1b45519ea20e02e2f6db2f7f769be97
Liu Chao <lc-soft@live.cn>
2013-09-14 21:51:23 +0800

    调整头文件包含顺序

--- include/LCUI/LCUI.h
+++ include/LCUI/LCUI.h
@@ -234,9 +234,9 @@ LCUI_END_HEADER
 	}
 
 #include LC_MISC_H
+#include LC_KERNEL_SLEEPER_H
 #include LC_KERNEL_MAIN_H
 #include LC_KERNEL_DEV_H
-#include LC_KERNEL_SLEEPER_H
 #include LC_KERNEL_TIMER_H
 #include LC_KERNEL_TASK_H
 #include LC_KERNEL_EVENT_H
