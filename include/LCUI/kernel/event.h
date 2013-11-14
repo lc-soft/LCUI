83a935fcc8b3915fb4e0b4defafc1b1e06287889
Liu Chao <lc-soft@live.cn>
2013-11-14 21:57:06 +0800

    移除函数声明

--- include/LCUI/kernel/event.h
+++ include/LCUI/kernel/event.h
@@ -107,11 +107,6 @@ LCUI_API LCUI_BOOL LCUI_PushEvent( LCUI_Event *event );
 /** 初始化事件槽记录 */
 LCUI_API void EventSlots_Init( LCUI_Queue *slots );
 
-/** 将函数指针以及两个参数，转换成LCUI_Func类型，保存至p_buff指向的缓冲区中 */
-LCUI_API LCUI_BOOL Get_FuncData(	LCUI_Func *p_buff,
-					void (*func) (void*,void*),
-					void *arg1, void *arg2 );
-
 /** 根据事件的ID，获取与该事件关联的事件槽 */
 LCUI_API LCUI_EventSlot* EventSlots_Find( LCUI_Queue *slots, int event_id );
 
