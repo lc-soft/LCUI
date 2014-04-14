c6496241bc0a4cadfdda1ddb5ea27d584e14f6a8
Liu Chao <lc-soft@live.cn>
2014-04-14 19:06:42 +0800

    将X轴和Y轴方向的模糊算法分离至单独的函数

--- src/draw/smooth.c
+++ src/draw/smooth.c
@@ -5,37 +5,104 @@
 
 #include <math.h>
 
+static const double PI = 3.141592653;
+
+/** X轴方向线性模糊 */
+static void HorizSmoothARGB( LCUI_ARGB *pSrcPixels, LCUI_ARGB *pDesPixels, 
+			     size_t len, double *kvalue, int kcenter )
+{
+	int j;
+	double i;
+	LCUI_ARGB *px_src, *px_des;
+	double sum, amul, bmul, gmul, rmul, *pkvalue;
+
+	for( i=0; i<len; ++i ) { 
+		sum = amul = bmul = gmul = rmul = 0;
+		px_src = pSrcPixels;
+		px_des = pDesPixels;
+		pkvalue = kvalue;
+		for( j=-kcenter; j<=kcenter; ++j ) {
+			if((i+j) >= 0 && (i+j) < len) {
+				bmul += px_src->b*(*pkvalue);
+				gmul += px_src->g*(*pkvalue);
+				rmul += px_src->r*(*pkvalue);
+				amul += px_src->a*(*pkvalue);
+				sum += (*pkvalue);
+			}
+			++px_src;
+			++pkvalue;
+		}
+		px_des->b = bmul/sum;
+		px_des->g = gmul/sum;
+		px_des->r = rmul/sum;
+		px_des->a = amul/sum;
+		++px_des;
+		++px_src;
+	}
+}
+
+/** Y轴方向线性模糊 */
+static void VertiSmoothARGB( LCUI_ARGB *pSrcPixels, LCUI_ARGB *pDesPixels, 
+		size_t rows, size_t cols, double *pkernel, int kcenter )
+{
+	int j;
+	double i;
+	LCUI_ARGB *px_src, *px_des;
+	double sum, amul, bmul, gmul, rmul, *pkvalue;
+	
+	for( i=0; i<rows; ++i ) { 
+		sum = amul = bmul = gmul = rmul = 0;
+		px_src = pSrcPixels;
+		px_des = pDesPixels;
+		pkvalue = pkernel;
+		for( j=-kcenter; j<=kcenter; ++j ) {
+			if((i+j) >= 0 && (i+j) < rows) {
+				bmul += px_src->b*(*pkvalue);
+				gmul += px_src->g*(*pkvalue);
+				rmul += px_src->r*(*pkvalue);
+				amul += px_src->a*(*pkvalue);
+				sum += (*pkvalue);
+			}
+			px_src += cols;
+			++pkvalue;
+		}
+		px_des->b = bmul/sum;
+		px_des->g = gmul/sum;
+		px_des->r = rmul/sum;
+		px_des->a = amul/sum;
+		px_des += cols;
+		px_src += cols;
+	}
+}
+
+/** 对图像进行高斯模糊处理 */
 LCUI_API int GaussianSmooth( LCUI_Graph *src, LCUI_Graph *des, double sigma )
-/* 对图像进行高斯模糊处理 */
 {
 	LCUI_Graph temp;
 	int kcenter, i;
 	int x, y, ksize;
-	int temp_pos, temp_start_pos;
-	int a_src_pos, b_src_pos, src_start_pos;
-	double amul, bmul, gmul, rmul, *kvalue_ptr;
 	double *kernel, scale, cons, sum = 0;
-	const double PI = 3.141592653;
+	LCUI_ARGB *p_src_px, *p_des_px;
 	
 	sigma = sigma > 0 ? sigma : -sigma;
-	//ksize为奇数 
+	// ksize为奇数 
 	ksize = ceil(sigma * 3) * 2 + 1; 
 	if(ksize == 1) {
 		Graph_Copy( des, src );
 		return 0;
 	}
 
-	//计算一维高斯核 
+	// 计算一维高斯核 
 	scale = -0.5/(sigma*sigma);
 	cons = 1/sqrt(-scale / PI); 
 	kcenter = ksize/2; 
-	kernel = (double*) malloc( ksize*sizeof(double) );
+	kernel = (double*)malloc( ksize*sizeof(double) );
 	for(i = 0; i < ksize; ++i) {
 		int x = i - kcenter;
-		*(kernel+i) = cons * exp(x * x * scale);//一维高斯函数
+		*(kernel+i) = cons * exp(x * x * scale); // 一维高斯函数
 		sum += *(kernel+i);
 	} 
-	//归一化,确保高斯权值在[0,1]之间
+	// 归一化,确保高斯权值在[0,1]之间
 	for(i = 0; i < ksize; i++) {
 		*(kernel+i) /= sum;
 	}
@@ -51,89 +118,31 @@ LCUI_API int GaussianSmooth( LCUI_Graph *src, LCUI_Graph *des, double sigma )
 		return -2;
 	}
 	
-	Graph_Lock( &temp );
-	
-	//x方向一维高斯模糊
-	temp_start_pos = 0;
-	src_start_pos = 0 - kcenter;
-	for(y = 0; y < src->h; y++) {
-		temp_pos = temp_start_pos;
-		b_src_pos = src_start_pos;
-		for(x = 0; x < src->w; x++) { 
-			sum = amul = bmul = gmul = rmul = 0;
-			a_src_pos = b_src_pos;
-			kvalue_ptr = kernel;
-			for(i = -kcenter; i <= kcenter; i++) {
-				if((x+i) >= 0 && (x+i) < src->w) {
-					rmul += src->rgba[0][a_src_pos]*(*kvalue_ptr);
-					gmul += src->rgba[1][a_src_pos]*(*kvalue_ptr);
-					bmul += src->rgba[2][a_src_pos]*(*kvalue_ptr);
-					if(src->color_type == COLOR_TYPE_RGBA) {
-						amul += src->rgba[3][a_src_pos]*(*kvalue_ptr);
-					}
-					sum += (*kvalue_ptr);
-				}
-				++a_src_pos;
-				++kvalue_ptr;
-			}
-			temp.rgba[0][temp_pos] = rmul/sum;
-			temp.rgba[1][temp_pos] = gmul/sum;
-			temp.rgba[2][temp_pos] = bmul/sum;
-			if(temp.color_type == COLOR_TYPE_RGBA) {
-				temp.rgba[3][temp_pos] = amul/sum;
-			}
-			++temp_pos;
-			++b_src_pos;
-		}
-		temp_start_pos += temp.w;
-		src_start_pos += src->w;
+	// x方向一维高斯模糊
+	p_des_px = temp.argb;
+	p_src_px = src->argb - kcenter;
+	for( y=0; y<src->h; ++y ) {
+		HorizSmoothARGB( p_src_px, p_des_px, src->w, kernel, kcenter );
+		p_des_px += temp.w;
+		p_src_px += src->w;
 	}
-	Graph_Unlock( &temp );
-	
-	Graph_Lock( &temp );
-	//y方向一维高斯模糊
-	src_start_pos = 0 - kcenter*temp.w;
-	src_start_pos = 0 - kcenter*temp.w;
-	for(x = 0; x < temp.w; x++) {
-		temp_start_pos = x;
-		b_src_pos = src_start_pos;
-		for(y = 0; y < temp.h; y++) { 
-			sum = amul = bmul = gmul = rmul = 0;
-			a_src_pos = b_src_pos;
-			kvalue_ptr = kernel;
-			for(i = -kcenter; i <= kcenter; i++) {
-				if((y+i) >= 0 && (y+i) < temp.h) { 
-					rmul += temp.rgba[0][a_src_pos]*(*kvalue_ptr);
-					gmul += temp.rgba[1][a_src_pos]*(*kvalue_ptr);
-					bmul += temp.rgba[2][a_src_pos]*(*kvalue_ptr);
-					if(temp.color_type == COLOR_TYPE_RGBA) {
-						amul += temp.rgba[3][a_src_pos]*(*kvalue_ptr);
-					}
-					sum += (*kvalue_ptr);
-				}
-				a_src_pos += temp.w;
-				++kvalue_ptr;
-			}
-			des->rgba[0][temp_start_pos] = rmul/sum;
-			des->rgba[1][temp_start_pos] = gmul/sum;
-			des->rgba[2][temp_start_pos] = bmul/sum;
-			if(des->color_type == COLOR_TYPE_RGBA) {
-				des->rgba[3][temp_start_pos] = amul/sum; 
-			}
-			temp_start_pos += des->w;
-			b_src_pos += temp.w;
-		}
-		++src_start_pos;
+
+	// y方向一维高斯模糊
+	p_des_px = temp.argb;
+	p_src_px = src->argb - kcenter*temp.w;
+	for( x=0; x<temp.w; ++x ) {
+		VertiSmoothARGB( p_src_px, p_des_px, src->h, src->w, kernel, kcenter );
+		++p_des_px;
+		++p_src_px;
 	}
 	
-	Graph_Unlock( &temp );
 	Graph_Free( &temp );
 	free(kernel);
 	return 0;
 }
 
+/** 对图像进行模糊处理 */
 LCUI_API int Graph_Smooth( LCUI_Graph *src, LCUI_Graph *des, double sigma )
-/* 对图像进行模糊处理 */
 {
 	return GaussianSmooth( src, des, sigma );
 }
