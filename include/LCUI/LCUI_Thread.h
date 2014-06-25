c6449ef3c0ecce2b9a65b1ad01cc669eb53ee6d2
Liu Chao <lc-soft@live.cn>
2014-06-25 23:25:46 +0800

    移除_LCUIThread_*系列函数

--- include/LCUI/LCUI_Thread.h
+++ include/LCUI/LCUI_Thread.h
@@ -59,81 +59,33 @@ typedef unsigned int LCUI_Thread;
 LCUI_BEGIN_HEADER
 
 /* init the mutex */
-LCUI_API int
-LCUIMutex_Init( LCUI_Mutex *mutex );
+LCUI_API int LCUIMutex_Init( LCUI_Mutex *mutex );
 
 /* Free the mutex */
-LCUI_API void
-LCUIMutex_Destroy( LCUI_Mutex *mutex );
+LCUI_API void LCUIMutex_Destroy( LCUI_Mutex *mutex );
 
 /* Try lock the mutex */
-LCUI_API int
-LCUIMutex_TryLock( LCUI_Mutex *mutex );
+LCUI_API int LCUIMutex_TryLock( LCUI_Mutex *mutex );
 
 /* Lock the mutex */
-LCUI_API int
-LCUIMutex_Lock( LCUI_Mutex *mutex );
+LCUI_API int LCUIMutex_Lock( LCUI_Mutex *mutex );
 
 /* Unlock the mutex */
-LCUI_API int
-LCUIMutex_Unlock( LCUI_Mutex *mutex );
+LCUI_API int LCUIMutex_Unlock( LCUI_Mutex *mutex );
 
-
-LCUI_API int
-_LCUIThread_Create( LCUI_Thread *thread, void(*func)(void*), void *arg );
-
-LCUI_API LCUI_Thread
-LCUIThread_SelfID( void );
-
-LCUI_API void
-_LCUIThread_Exit( void *retval );
-
-LCUI_API void
-_LCUIThread_Cancel( LCUI_Thread thread );
-
-LCUI_API int
-_LCUIThread_Join( LCUI_Thread thread, void **retval );
-
-
-/* 获取指定线程的根线程ID */
-LCUI_API LCUI_Thread
-LCUIThread_GetRootThreadID( LCUI_Thread tid );
-
-/* 打印各个线程的信息 */
-LCUI_API void
-LCUIThread_PrintInfo( void );
+LCUI_API LCUI_Thread LCUIThread_SelfID( void );
 
 /* 创建并运行一个线程 */
-LCUI_API int
-LCUIThread_Create( LCUI_Thread *tidp, void (*start_rtn)(void*), void * arg );
+LCUI_API int LCUIThread_Create( LCUI_Thread *tidp, void (*start_rtn)(void*), void *arg );
 
 /* 等待一个线程的结束，并释放该线程的资源 */
-LCUI_API int
-LCUIThread_Join( LCUI_Thread thread, void **retval );
+LCUI_API int LCUIThread_Join( LCUI_Thread thread, void **retval );
 
 /* 撤销一个线程 */
-LCUI_API void
-LCUIThread_Cancel( LCUI_Thread thread );
+LCUI_API void LCUIThread_Cancel( LCUI_Thread thread );
 
 /* 记录指针作为返回值，并退出线程 */
-LCUI_API void
-LCUIThread_Exit( void* retval );
-
-/* 撤销指定ID的程序的全部子线程 */
-LCUI_API int
-LCUIApp_CancelAllThreads( LCUI_ID app_id );
-
-/* 注册程序主线程 */
-LCUI_API void
-LCUIApp_RegisterMainThread( LCUI_ID app_id );
-
-/* 初始化线程模块 */
-LCUI_API void
-LCUIModule_Thread_Init( void );
-
-/* 停用线程模块 */
-LCUI_API void
-LCUIModule_Thread_End( void );
+LCUI_API void LCUIThread_Exit( void* retval );
 
 LCUI_END_HEADER
 
