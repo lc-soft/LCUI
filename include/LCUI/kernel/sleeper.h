d28570e188df6959d7339d5eeff1bfe5f3759c55
Liu Chao <lc-soft@live.cn>
2014-07-02 21:54:35 +0800

    LCUI_Sleeper 更名为 LCUI_Cond，调整相关函数命名
    Sleeper更名为 Cond，睡眠的目的是为了等待条件成立，名字改为Cond更好些。
    
    之前在GNU/Linux环境下是用sem实现LCUI_Sleeper的功能，但sem_post()一次只能唤醒单个线程，因此，之前的版本在GNU/Linux环境下运行会有问题。
    已经移除GNU/Linux环境下的实现代码，等待添加。

--- include/LCUI/kernel/sleeper.h
+++ include/LCUI/kernel/sleeper.h
@@ -1,47 +1,41 @@
-﻿/** ******************************************************************************
- * @file	sleeper.h
- * @brief	a sleep function, can be break
- * @author	Liu Chao <lc-soft@live.cn>
- * @warning
- * Copyright (C) 2012-2013 by							\n
- * Liu Chao									\n
- * 										\n
- * This file is part of the LCUI project, and may only be used, modified, and	\n
- * distributed under the terms of the GPLv2.					\n
- * 										\n
- * (GPLv2 is abbreviation of GNU General Public License Version 2)		\n
- * 										\n
- * By continuing to use, modify, or distribute this file you indicate that you	\n
- * have read the license and understand and accept it fully.			\n
- *  										\n
- * The LCUI project is distributed in the hope that it will be useful, but 	\n
- * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 	\n
- * or FITNESS FOR A PARTICULAR PURPOSE. See the GPL v2 for more details.	\n
- * 										\n
- * You should have received a copy of the GPLv2 along with this file. It is 	\n
- * usually in the LICENSE.TXT file, If not, see <http://www.gnu.org/licenses/>.	\n
- * ******************************************************************************/
+﻿/* ***************************************************************************
+ * cond.h -- condition variables, for the mechanism of thread synchronization
+ * 
+ * Copyright (C) 2013-2014 by Liu Chao <lc-soft@live.cn>
+ * 
+ * This file is part of the LCUI project, and may only be used, modified, and
+ * distributed under the terms of the GPLv2.
+ * 
+ * (GPLv2 is abbreviation of GNU General Public License Version 2)
+ * 
+ * By continuing to use, modify, or distribute this file you indicate that you
+ * have read the license and understand and accept it fully.
+ *  
+ * The LCUI project is distributed in the hope that it will be useful, but 
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GPL v2 for more details.
+ * 
+ * You should have received a copy of the GPLv2 along with this file. It is 
+ * usually in the LICENSE.TXT file, If not, see <http://www.gnu.org/licenses/>.
+ * ***************************************************************************/
  
-/** ******************************************************************************
- * @file	sleeper.h
- * @brief	可被打断的睡眠功能
- * @author	刘超 <lc-soft@live.cn>
- * @warning
- * 版权所有 (C) 2012-2013 归属于						\n
- * 刘超										\n
- * 										\n
- * 这个文件是LCUI项目的一部分，并且只可以根据GPLv2许可协议来使用、更改和发布。	\n
- * 										\n
- * (GPLv2 是 GNU通用公共许可证第二版 的英文缩写)				\n
- * 										\n
- * 继续使用、修改或发布本文件，表明您已经阅读并完全理解和接受这个许可协议。	\n
- * 										\n
- * LCUI 项目是基于使用目的而加以散布的，但不负任何担保责任，甚至没有适销性或特定\n
- * 用途的隐含担保，详情请参照GPLv2许可协议。					\n
- * 										\n
- * 您应已收到附随于本文件的GPLv2许可协议的副本，它通常在LICENSE.TXT文件中，如果	\n
- * 没有，请查看：<http://www.gnu.org/licenses/>. 				\n
- * ******************************************************************************/
+/* ****************************************************************************
+ * cond.h -- 条件变量，用于进行线程同步的机制
+ *
+ * 版权所有 (C) 2013-2014 归属于 刘超 <lc-soft@live.cn>
+ * 
+ * 这个文件是LCUI项目的一部分，并且只可以根据GPLv2许可协议来使用、更改和发布。
+ *
+ * (GPLv2 是 GNU通用公共许可证第二版 的英文缩写)
+ * 
+ * 继续使用、修改或发布本文件，表明您已经阅读并完全理解和接受这个许可协议。
+ * 
+ * LCUI 项目是基于使用目的而加以散布的，但不负任何担保责任，甚至没有适销性或特
+ * 定用途的隐含担保，详情请参照GPLv2许可协议。
+ *
+ * 您应已收到附随于本文件的GPLv2许可协议的副本，它通常在LICENSE.TXT文件中，如果
+ * 没有，请查看：<http://www.gnu.org/licenses/>. 
+ * ***************************************************************************/
 
 #ifndef __LC_KERNEL_SLEEPER_H__
 #define __LC_KERNEL_SLEEPER_H__
@@ -50,21 +44,22 @@ LCUI_BEGIN_HEADER
 
 #ifdef LCUI_BUILD_IN_WIN32
 #include <windows.h>
-typedef HANDLE LCUI_Sleeper;
+typedef HANDLE LCUI_Cond;
 #else
-#include <semaphore.h>
-typedef sem_t LCUI_Sleeper;
+// ...
 #endif
 
-/** 新建一个睡眠者 */
-LCUI_API int LCUISleeper_Create( LCUI_Sleeper *p_sleeper );
+/** 新建一个条件变量 */
+LCUI_API int LCUICond_Init( LCUI_Cond *cond );
 
-/** 让指定睡眠者睡一段时间 */
-LCUI_API unsigned int LCUISleeper_StartSleep(	LCUI_Sleeper *sleeper,
-						unsigned int ms );
+/** 阻塞当前线程，等待条件成立 */
+LCUI_API unsigned int LCUICond_Wait( LCUI_Cond *cond );
 
-/** 打断目标睡眠者的睡眠 */
-LCUI_API int LCUISleeper_BreakSleep( LCUI_Sleeper *sleeper );
+/** 计时阻塞当前线程，等待条件成立 */
+LCUI_API unsigned int LCUICond_TimedWait( LCUI_Cond *cond, unsigned int ms );
+
+/** 唤醒所有阻塞等待条件成立的线程 */
+LCUI_API int LCUICond_Broadcast( LCUI_Cond *cond );
 
 LCUI_END_HEADER
 
--- src/kernel/sleeper.c
+++ src/kernel/sleeper.c
@@ -1,55 +1,47 @@
-﻿/** ******************************************************************************
- * @file	sleeper.c
- * @brief	a sleep function, can be break
- * @author	Liu Chao <lc-soft@live.cn>
- * @warning
- * Copyright (C) 2012-2013 by							\n
- * Liu Chao									\n
- * 										\n
- * This file is part of the LCUI project, and may only be used, modified, and	\n
- * distributed under the terms of the GPLv2.					\n
- * 										\n
- * (GPLv2 is abbreviation of GNU General Public License Version 2)		\n
- * 										\n
- * By continuing to use, modify, or distribute this file you indicate that you	\n
- * have read the license and understand and accept it fully.			\n
- *  										\n
- * The LCUI project is distributed in the hope that it will be useful, but 	\n
- * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 	\n
- * or FITNESS FOR A PARTICULAR PURPOSE. See the GPL v2 for more details.	\n
- * 										\n
- * You should have received a copy of the GPLv2 along with this file. It is 	\n
- * usually in the LICENSE.TXT file, If not, see <http://www.gnu.org/licenses/>.	\n
- * ******************************************************************************/
+﻿/* ***************************************************************************
+ * cond.c -- condition variables, for the mechanism of thread synchronization
+ * 
+ * Copyright (C) 2013-2014 by Liu Chao <lc-soft@live.cn>
+ * 
+ * This file is part of the LCUI project, and may only be used, modified, and
+ * distributed under the terms of the GPLv2.
+ * 
+ * (GPLv2 is abbreviation of GNU General Public License Version 2)
+ * 
+ * By continuing to use, modify, or distribute this file you indicate that you
+ * have read the license and understand and accept it fully.
+ *  
+ * The LCUI project is distributed in the hope that it will be useful, but 
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GPL v2 for more details.
+ * 
+ * You should have received a copy of the GPLv2 along with this file. It is 
+ * usually in the LICENSE.TXT file, If not, see <http://www.gnu.org/licenses/>.
+ * ***************************************************************************/
  
-/** ******************************************************************************
- * @file	sleeper.c
- * @brief	可被打断的睡眠功能
- * @author	刘超 <lc-soft@live.cn>
- * @warning
- * 版权所有 (C) 2012-2013 归属于						\n
- * 刘超										\n
- * 										\n
- * 这个文件是LCUI项目的一部分，并且只可以根据GPLv2许可协议来使用、更改和发布。	\n
- * 										\n
- * (GPLv2 是 GNU通用公共许可证第二版 的英文缩写)				\n
- * 										\n
- * 继续使用、修改或发布本文件，表明您已经阅读并完全理解和接受这个许可协议。	\n
- * 										\n
- * LCUI 项目是基于使用目的而加以散布的，但不负任何担保责任，甚至没有适销性或特定\n
- * 用途的隐含担保，详情请参照GPLv2许可协议。					\n
- * 										\n
- * 您应已收到附随于本文件的GPLv2许可协议的副本，它通常在LICENSE.TXT文件中，如果	\n
- * 没有，请查看：<http://www.gnu.org/licenses/>. 				\n
- * ******************************************************************************/
+/* ****************************************************************************
+ * cond.c -- 条件变量，用于进行线程同步的机制
+ *
+ * 版权所有 (C) 2013-2014 归属于 刘超 <lc-soft@live.cn>
+ * 
+ * 这个文件是LCUI项目的一部分，并且只可以根据GPLv2许可协议来使用、更改和发布。
+ *
+ * (GPLv2 是 GNU通用公共许可证第二版 的英文缩写)
+ * 
+ * 继续使用、修改或发布本文件，表明您已经阅读并完全理解和接受这个许可协议。
+ * 
+ * LCUI 项目是基于使用目的而加以散布的，但不负任何担保责任，甚至没有适销性或特
+ * 定用途的隐含担保，详情请参照GPLv2许可协议。
+ *
+ * 您应已收到附随于本文件的GPLv2许可协议的副本，它通常在LICENSE.TXT文件中，如果
+ * 没有，请查看：<http://www.gnu.org/licenses/>. 
+ * ***************************************************************************/
 
 #include <LCUI_Build.h>
-#include LC_LCUI_H
-#ifdef LCUI_BUILD_IN_LINUX
-#include <errno.h>
-#endif
-/** 新建一个睡眠者 */
-LCUI_API int LCUISleeper_Create( LCUI_Sleeper *p_sleeper )
+#include <LCUI/LCUI.h>
+
+/** 初始化一个条件变量 */
+int LCUICond_Init( LCUI_Cond *cond )
 {
 #ifdef LCUI_BUILD_IN_WIN32
 	/* 创建一个事件对象：
@@ -58,25 +50,43 @@ LCUI_API int LCUISleeper_Create( LCUI_Sleeper *p_sleeper )
 	 * 初始状态为无信号状态
 	 * 不指定名字，无名
 	 */
-	*p_sleeper = CreateEvent( NULL, FALSE, FALSE, NULL );
-	if( *p_sleeper == 0 ) {
+	*cond = CreateEvent( NULL, FALSE, FALSE, NULL );
+	if( *cond == 0 ) {
 		return -1;
 	}
 	return 0;
 #else
-	return sem_init( p_sleeper, 0, 0 );
+	// ...
 #endif
 }
 
-/** 让指定睡眠者睡一段时间 */
-LCUI_API unsigned int LCUISleeper_StartSleep(	LCUI_Sleeper* sleeper,
-						unsigned int ms )
+/** 阻塞当前线程，等待条件成立 */
+unsigned int LCUICond_Wait( LCUI_Cond *cond )
 {
 #ifdef LCUI_BUILD_IN_WIN32
 	int ret;
 	int64_t lost_time;
 	lost_time = LCUI_GetTickCount();
-	ret = WaitForSingleObject( *sleeper, ms );
+	ret = WaitForSingleObject( *cond, INFINITE );
+	lost_time = LCUI_GetTicks( lost_time );
+	switch( ret ) {
+	case WAIT_OBJECT_0: return (unsigned int)lost_time;
+	default: break;
+	}
+	return ret;
+#else
+	// ...
+#endif
+}
+
+/** 计时阻塞当前线程，等待条件成立 */
+unsigned int LCUICond_TimedWait( LCUI_Cond *cond, unsigned int ms )
+{
+#ifdef LCUI_BUILD_IN_WIN32
+	int ret;
+	int64_t lost_time;
+	lost_time = LCUI_GetTickCount();
+	ret = WaitForSingleObject( *cond, ms );
 	lost_time = LCUI_GetTicks( lost_time );
 	switch( ret ) {
 	case WAIT_OBJECT_0: return (unsigned int)lost_time;
@@ -86,34 +96,19 @@ LCUI_API unsigned int LCUISleeper_StartSleep(	LCUI_Sleeper* sleeper,
 	}
 	return ret;
 #else
-	int ret;
-	int64_t lost_time;
-	struct timespec ts;
-
-	clock_gettime(CLOCK_REALTIME, &ts );
-	ts.tv_sec += ms/1000;
-	ts.tv_nsec += ms%1000*1000*1000;
-	lost_time = LCUI_GetTickCount();
-	ret = sem_timedwait( sleeper, &ts );
-	lost_time = LCUI_GetTicks( lost_time );
-	switch( ret ) {
-	case 0: return (unsigned int)lost_time;
-	case ETIMEDOUT: return ms;
-	default: break;
-	}
-	return ret;
+	// ...
 #endif
 }
 
-/** 打断目标睡眠者的睡眠 */
-LCUI_API int LCUISleeper_BreakSleep( LCUI_Sleeper* sleeper )
+/** 唤醒所有阻塞等待条件成立的线程 */
+int LCUICond_Broadcast( LCUI_Cond* cond )
 {
 #ifdef LCUI_BUILD_IN_WIN32
-	if( SetEvent( *sleeper ) ) {
+	if( SetEvent( *cond ) ) {
 		return 0;
 	}
 	return -1;
 #else
-	return sem_post( sleeper );
+	// ...
 #endif
 }
