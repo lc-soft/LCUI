bf20e9bb2180b304c42d69a1d14102814f269ad9
Liu Chao <lc-soft@live.cn>
2014-11-11 12:58:51 +0800

    更新 autogen.sh

--- autogen.sh
+++ autogen.sh
@@ -5,6 +5,16 @@ test -z "$srcdir" && srcdir=.
 
 DIE=0
 
+# On MacOS X, the GNU libtool is named `glibtool'.
+HOSTOS=`uname`
+if test "$LIBTOOLIZE"x != x; then
+  :
+elif test "$HOSTOS"x = Darwinx; then
+  LIBTOOLIZE=glibtoolize
+else
+  LIBTOOLIZE=libtoolize
+fi
+
 (test -f $srcdir/configure.ac) || {
 	echo "**Error**: Directory "\`$srcdir\'" does not look like the top-level package directory"
 	exit 1
@@ -16,12 +26,10 @@ DIE=0
 	DIE=1
 }
 
-(libtoolize --version) < /dev/null > /dev/null 2>&1 || {
-     (glibtoolize --version) < /dev/null > /dev/null 2>&1 || {
-         echo
-         echo "**Error**: You must have \`libtool' installed."
-         DIE=1
-     }
+($LIBTOOLIZE --version) < /dev/null > /dev/null 2>&1 || {
+	echo
+	echo "**Error**: You must have \`libtool' installed."
+	DIE=1
 }
 
 (pkg-config --version) < /dev/null > /dev/null 2>&1 || {
@@ -35,7 +43,6 @@ DIE=0
 	echo
 	echo "**Error**: You must have \`automake' (1.7 or later) installed."
 	DIE=1
-	NO_AUTOMAKE=yes
 }
 
 
@@ -51,7 +58,7 @@ if test "$DIE" -eq 1; then
 	exit 1
 fi
 
-libtoolize || glibtoolize
-aclocal -I m4
+$LIBTOOLIZE --force --copy --install
+aclocal -I m4 --force
 automake --add-missing
-autoconf
+autoconf --force
