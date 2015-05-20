cd5c79a6ad631447c641e993e1e801ecf0036a6b
Liu Chao <lc-soft@live.cn>
2015-05-20 22:06:54 +0800

    更新背景图和阴影测试代码

--- test/helloworld.c
+++ test/helloworld.c
@@ -37,44 +37,53 @@ void onTimer( void *arg )
 
 int main( int argc, char **argv )
 {
-	LCUI_Widget w;
-	LCUI_Graph image;
+	LCUI_Widget w, root;
+	LCUI_Graph desktop_image;
 
 	InitConsoleWindow();
 	LCUI_Init();
 	LCUIDisplay_SetMode( LDM_WINDOWED );
 	LCUIDisplay_SetSize( 800, 600 );
-	w = Widget_New(NULL);
+	w = LCUIWidget_New("debug-widget");
 	Widget_Top( w );
 	Widget_Show( w );
 	Widget_Resize( w, 320, 240 );
-	Widget_Move( w, 100, 200 );
+	Widget_Move( w, 200, 200 );
 	Widget_SetTitleW( w, L"测试" );
-	Graph_Init( &image ); 
-	_DEBUG_MSG( "load image, result: %d\n", Graph_LoadImage( "background-image.png", &image ) );
+	Graph_Init( &desktop_image );
+	_DEBUG_MSG("load image, result: %d\n",  Graph_LoadImage( "images/background-image.png", &desktop_image ));
+	root = LCUIWidget_GetRoot();
+
+	Widget_PullStyle( root, WSS_BACKGROUND );
+	root->style.background.color = RGB(255,242,223);
+	root->style.background.image = desktop_image;
+	root->style.background.size.using_value = TRUE;
+	root->style.background.size.value = SV_CONTAIN;
+	Widget_PushStyle( root, WSS_BACKGROUND );
+
 	Widget_PullStyle( w, WSS_BACKGROUND | WSS_SHADOW | WSS_BORDER );
-	w->style.background.color.value = 0xf6f6f6;
-	w->style.background.image = image; 
-	w->style.background.size.w.scale = 0.50;
+	w->style.background.color.value = 0xccffffff;
+	w->style.background.size.w.scale = 0.50; 
 	w->style.background.size.h.px = 200;
 	w->style.background.size.w.type = SVT_SCALE;
 	w->style.background.size.h.type = SVT_PX;
 	w->style.background.size.using_value = FALSE;
 	w->style.background.position.using_value = TRUE;
 	w->style.background.position.value = SV_BOTTOM_CENTER;
-	w->style.shadow.color = ARGB( 50, 0, 0, 0 );
-	w->style.shadow.x = 1;
-	w->style.shadow.y = 1;
-	w->style.shadow.spread = 5;
-	w->style.shadow.blur = 5;
-	w->style.border.top.width = 4;
-	w->style.border.top.color = RGB( 255, 0, 0 );
-	w->style.border.right.width = 6;
-	w->style.border.right.color = RGB( 0, 255, 0 );
-	w->style.border.bottom.width = 8;
-	w->style.border.bottom.color = RGB( 0, 0, 255 );
-	w->style.border.left.width = 10;
+	w->style.shadow.color = ARGB(200,0,122,204);
+	w->style.shadow.x = 2;
+	w->style.shadow.y = 2;
+	w->style.shadow.spread = 0;
+	w->style.shadow.blur = 8;
+	w->style.border.top.width = 1;
+	w->style.border.right.width = 1;
+	w->style.border.bottom.width = 1;
+	w->style.border.left.width = 1;
+	w->style.border.top.color = RGB(0,122,204);
+	w->style.border.right.color = RGB(0,122,204);
+	w->style.border.bottom.color = RGB(0,122,204);
+	w->style.border.left.color = RGB(0,122,204);
 	Widget_PushStyle( w, WSS_BACKGROUND | WSS_SHADOW | WSS_BORDER );
-	LCUITimer_Set( 2000, onTimer, NULL, TRUE );
+	//LCUITimer_Set( 5000, onTimer, NULL, TRUE );
 	return LCUI_Main();
 }
