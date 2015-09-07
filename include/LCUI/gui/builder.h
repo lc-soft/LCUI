a501ec72c278b5618f1f8b86d988506a4df2f5d1
Liu Chao <lc-soft@live.cn>
2015-09-07 21:33:07 +0800

    添加 builder.c 模块

---
+++ include/LCUI/gui/builder.h
@@ -0,0 +1,61 @@
+﻿/* ***************************************************************************
+ * builder.h -- the GUI build module, parse UI config code and build UI.
+ *
+ * Copyright (C) 2015 by Liu Chao <lc-soft@live.cn>
+ *
+ * This file is part of the LCUI project, and may only be used, modified, and
+ * distributed under the terms of the GPLv2.
+ *
+ * (GPLv2 is abbreviation of GNU General Public License Version 2)
+ *
+ * By continuing to use, modify, or distribute this file you indicate that you
+ * have read the license and understand and accept it fully.
+ *
+ * The LCUI project is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GPL v2 for more details.
+ *
+ * You should have received a copy of the GPLv2 along with this file. It is
+ * usually in the LICENSE.TXT file, If not, see <http://www.gnu.org/licenses/>.
+ * ****************************************************************************/
+
+/* ****************************************************************************
+ * builder.h -- 图形界面构建模块，主要用于解析界面配置代码并生成相应的图形界面
+ *
+ * 版权所有 (C) 2015 归属于 刘超 <lc-soft@live.cn>
+ *
+ * 这个文件是LCUI项目的一部分，并且只可以根据GPLv2许可协议来使用、更改和发布。
+ *
+ * (GPLv2 是 GNU通用公共许可证第二版 的英文缩写)
+ *
+ * 继续使用、修改或发布本文件，表明您已经阅读并完全理解和接受这个许可协议。
+ *
+ * LCUI 项目是基于使用目的而加以散布的，但不负任何担保责任，甚至没有适销性或特
+ * 定用途的隐含担保，详情请参照GPLv2许可协议。
+ *
+ * 您应已收到附随于本文件的GPLv2许可协议的副本，它通常在LICENSE.TXT文件中，如果
+ * 没有，请查看：<http://www.gnu.org/licenses/>.
+ * ****************************************************************************/
+
+#ifndef __LCUI_BUILDER_H__
+#define __LCUI_BUILDER_H__
+
+LCUI_BEGIN_HEADER
+
+/**
+ * 从字符串中载入界面配置代码，解析并生成相应的图形界面(元素)
+ * @param[in] str 包含界面配置代码的字符串
+ * @return 正常解析会返回 0，出现错误则返回 -1
+ */
+LCUI_API int LCUIBuilder_LoadString( const char *str );
+
+/**
+ * 从文件中载入界面配置代码，解析并生成相应的图形界面(元素)
+ * @param[in] filepath 文件路径
+ * @return 正常解析会返回 0，出现错误则返回 -1
+ */
+LCUI_API int LCUIBuilder_Load( const char *filepath );
+
+LCUI_END_HEADER
+
+#endif
