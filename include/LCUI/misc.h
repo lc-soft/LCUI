4b9a920d7b91aaf795215aea0830a4222d8e9a94
Liu Chao <lc-soft@live.cn>
2015-12-24 22:55:49 +0800

    移除 misc.c

--- include/LCUI/misc.h
+++ include/LCUI/misc.h
@@ -39,53 +39,6 @@
 
 #ifndef __LCUI_MISC_H__
 #define __LCUI_MISC_H__
-
-LCUI_BEGIN_HEADER
-
-/** 对齐方式 */
-enum AlignType {
-	ALIGN_NONE,		/* 无 */
-	ALIGN_TOP_LEFT,	  	/* 向左上角对齐 */
-	ALIGN_TOP_CENTER,	/* 向上中间对齐 */
-	ALIGN_TOP_RIGHT,	/* 向右上角对齐 */
-	ALIGN_MIDDLE_LEFT,	/* 向中央偏左对齐 */
-	ALIGN_MIDDLE_CENTER,	/* 向正中央对齐 */
-	ALIGN_MIDDLE_RIGHT,	/* 向中央偏由对齐 */
-	ALIGN_BOTTOM_LEFT,	/* 向底部偏左对齐 */
-	ALIGN_BOTTOM_CENTER,	/* 向底部居中对齐 */
-	ALIGN_BOTTOM_RIGHT	/* 向底部偏右对齐 */
-};
-
-/** 布局方式 */
-enum LayoutType {
-	LAYOUT_NONE	= 0,	  /* 无 */
-	LAYOUT_NORMAL	= 0,
-	LAYOUT_ZOOM	= 1,	  /* 缩放 */
-	LAYOUT_STRETCH	= 1<<1,	  /* 拉伸 */
-	LAYOUT_CENTER	= 1<<2,	  /* 居中 */
-	LAYOUT_TILE	= 1<<3	  /* 平铺 */
-};
-
-/** 转换成LCUI_Pos类型 */
-LCUI_Pos Pos(int x, int y);
-
-/** 转换成LCUI_Size类型 */
-LCUI_Size Size(int w, int h);
-
-/* 
- * 功能：对比两个尺寸
- * 说明：a大于b，返回1， b大于a，返回-1，相等则返回0
- * */
-int Size_Cmp( LCUI_Size a, LCUI_Size b );
-
-/** 根据容器尺寸，区域尺寸以及对齐方式，获取该区域的位置 */
-LCUI_Pos GetPosByAlign( LCUI_Size box, LCUI_Size child, int align );
-
-/** 根据给定的字符串，生成一个ID */
-unsigned int BKDRHash( const char *str );
-
-LCUI_END_HEADER
-
 #include <LCUI/misc/delay.h>
 #include <LCUI/misc/dirent.h>
 #include <LCUI/misc/rbtree.h>
@@ -94,5 +47,4 @@ LCUI_END_HEADER
 #include <LCUI/misc/framectrl.h>
 #include <LCUI/misc/string.h>
 #include <LCUI/misc/parse.h>
-
 #endif
