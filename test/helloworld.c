50c4f80dad40251b62ae5d7e9ae52e61944f8be0
Liu Chao <lc-soft@live.cn>
2016-07-11 18:37:02 +0800

    更新 helloworld 程序

--- test/helloworld.c
+++ test/helloworld.c
@@ -2,43 +2,31 @@
 #include <LCUI/LCUI.h>
 #include <LCUI/gui/widget.h>
 #include <LCUI/gui/builder.h>
-
-int main( int argc, char **argv )
-{
-	LCUI_Widget root, pack;
-	LCUI_Init();
-	root = LCUIWidget_GetRoot();
-	pack = LCUIBuilder_LoadFile( "helloworld.xml" );
-	if( !pack ) {
-		return -1;
-	}
-	Widget_Append( root, pack ); 
-	Widget_Unwrap( pack );
-	return LCUI_Main();
-}
-
-/* version 2
-#include <LCUI_Build.h>
-#include <LCUI/LCUI.h>
-#include <LCUI/gui/widget.h>
 #include <LCUI/gui/widget/textview.h>
+#include <LCUI/gui/widget/textedit.h>
+
+static void OnBtnClick( LCUI_Widget self, LCUI_WidgetEvent e, void *arg )
+{
+        wchar_t str[256];
+        LCUI_Widget edit = LCUIWidget_GetById( "edit" );
+        LCUI_Widget txt = LCUIWidget_GetById( "text-hello" );
+        TextEdit_GetTextW( edit, 0, 255, str );
+        TextView_SetTextW( txt, str );
+}
 
 int main( int argc, char **argv )
 {
-	LCUI_Widget root, txt;
+        LCUI_Widget root, pack, btn;
 
-	LCUI_Init();
-	root = LCUIWidget_GetRoot();
-	txt = LCUIWidget_New( "textview" );
-	TextView_SetTextAlign( txt, SV_CENTER );
-	TextView_SetText( txt, "[size=18px]Hello, World![/size]" );
-	SetStyle( txt->custom_style, key_margin_top, 25, px );
-	SetStyle( txt->custom_style, key_margin_left, 25, px );
-	SetStyle( txt->custom_style, key_padding_top, 25, px );
-	SetStyle( txt->custom_style, key_border_width, 1, px );
-	SetStyle( txt->custom_style, key_border_color, RGB(0,0,0), color );
-	Widget_Resize( txt, 200, 100 );
-	Widget_Append( root, txt ); 
-	return LCUI_Main();
+        LCUI_Init();
+        root = LCUIWidget_GetRoot();
+        pack = LCUIBuilder_LoadFile( "helloworld.xml" );
+        if( !pack ) {
+                return -1;
+        }
+        Widget_Append( root, pack ); 
+        Widget_Unwrap( pack );
+        btn = LCUIWidget_GetById( "btn-ok" );
+        Widget_BindEvent( btn, "click", OnBtnClick, NULL, NULL );
+        return LCUI_Main();
 }
-*/
