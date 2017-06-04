161c969b7a7402f9298214502c73508362a1aa50
Liu Chao <lc-soft@live.cn>
2017-06-04 23:51:59 +0800

    Update test cases, add widget layout test. #90

--- test/test.h
+++ test/test.h
@@ -1,9 +1,22 @@
 ﻿
-#define assert(X) if(!(X)) {_DEBUG_MSG("nopass\n");return -1;}
+extern int tests_count;
+
+#define TEST_LOG(format, ...)\
+Logger_Log("[test] %s(): "format, __FUNCTION__ , ##__VA_ARGS__)
+
+#define PRINT_TEST_RESULT(N)\
+Logger_Log("[test] %d tests, %d pass.\n", tests_count, tests_count + N)
+
+#define CHECK(X) do {\
+	tests_count += 1;\
+	LOG("[test] %s(): %s. # %s\n", __FUNCTION__, ""#X"",\
+	    (X) ? "PASS" : (ret -= 1, "NO PASS!"));\
+} while( 0 );
 
 int test_string( void );
 int test_css_parser( void );
 int test_char_render( void );
 int test_string_render( void );
 int test_widget_render( void );
+int test_widget_layout( void );
 int test_image_reader( void );
