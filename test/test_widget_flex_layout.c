4e1ff1f28ee14fc7c844cdcc8d0fd8008c005a1e
Liu Chao <lc-soft@live.cn>
2017-12-23 22:40:16 +0800

    Add test for flex layout in absolute position

--- test/test_widget_flex_layout.c
+++ test/test_widget_flex_layout.c
@@ -27,7 +27,7 @@ static int check_flex_box_1( void )
 	};
 	box = LCUIWidget_GetById( "flex-box-1" );
 	CHECK( box->x == 0 && box->y == 0 );
-	CHECK( box->height == BLOCK_SIZE * 3 );
+	CHECK( box->height == BLOCK_SIZE * 3 + 1 );
 	for( LinkedList_Each( node, &box->children ) ) {
 		child = node->data;
 		sprintf( str, "[%d] (%g, %g) == (%g, %g)", child->index,
@@ -56,8 +56,8 @@ static int check_flex_box_2( void )
 		{ offset_x2 + BLOCK_SIZE, BLOCK_SIZE * 2 }
 	};
 	box = LCUIWidget_GetById( "flex-box-2" );
-	CHECK( box->x == 0 && box->y == BLOCK_SIZE * 3 );
-	CHECK( box->height == BLOCK_SIZE * 3 );
+	CHECK( box->x == 0 && box->y == BLOCK_SIZE * 3 + 1 );
+	CHECK( box->height == BLOCK_SIZE * 3 + 1 );
 	for( LinkedList_Each( node, &box->children ) ) {
 		child = node->data;
 		sprintf( str, "[%d] (%g, %g) == (%g, %g)", child->index,
@@ -86,8 +86,34 @@ static int check_flex_box_3( void )
 		{ offset_x2 + BLOCK_SIZE, BLOCK_SIZE * 2 }
 	};
 	box = LCUIWidget_GetById( "flex-box-3" );
-	CHECK( box->x == 0 && box->y == BLOCK_SIZE * 3 * 2 );
-	CHECK( box->height == BLOCK_SIZE * 3 );
+	CHECK( box->x == 0 && box->y == BLOCK_SIZE * 3 * 2 + 2 );
+	CHECK( box->height == BLOCK_SIZE * 3 + 1 );
+	for( LinkedList_Each( node, &box->children ) ) {
+		child = node->data;
+		sprintf( str, "[%d] (%g, %g) == (%g, %g)", child->index,
+			 child->x, child->y, simples[i][0], simples[i][1] );
+		CHECK_WITH_TEXT( str, child->x == simples[i][0] &&
+				 child->y == simples[i][1] );
+		i += 1;
+	}
+	return ret;
+}
+
+static int check_flex_box_4( void )
+{
+	char str[256];
+	int ret = 0, i = 0;
+	LinkedListNode *node;
+	LCUI_Widget child, box;
+	const float offset_x = 480 - BLOCK_SIZE * 3;
+	float simples[][2] = {
+		{ 0, 0 },
+		{ offset_x, BLOCK_SIZE },
+		{ offset_x + BLOCK_SIZE, BLOCK_SIZE },
+		{ offset_x + BLOCK_SIZE * 2, BLOCK_SIZE }
+	};
+	box = LCUIWidget_GetById( "flex-box-4" );
+	CHECK( box->height == BLOCK_SIZE * 2 );
 	for( LinkedList_Each( node, &box->children ) ) {
 		child = node->data;
 		sprintf( str, "[%d] (%g, %g) == (%g, %g)", child->index,
@@ -105,6 +131,7 @@ static int check_layout( void )
 	ret += check_flex_box_1();
 	ret += check_flex_box_2();
 	ret += check_flex_box_3();
+	ret += check_flex_box_4();
 	return ret;
 }
 
