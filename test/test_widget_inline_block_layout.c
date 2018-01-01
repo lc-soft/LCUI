646cec73f82f8543838cd2dd3612790101c8c106
Liu Chao <lc-soft@live.cn>
2018-01-01 23:36:20 +0800

    Add dropdwon menu size check

--- test/test_widget_inline_block_layout.c
+++ test/test_widget_inline_block_layout.c
@@ -11,12 +11,25 @@
 #define SCREEN_WIDTH	680
 #define SCREEN_HEIGHT	480
 
+static int check_dropdown_menu( void )
+{
+	int ret = 0;
+	LCUI_Widget menu;
+	float height = 34 * 3 + 2 + 8 * 2;
+	menu = LCUIWidget_GetById( "test-dropdown" );
+	CHECK( menu->x == 20 && menu->y == SCREEN_HEIGHT - height - 20 );
+	CHECK( menu->width > 220 && menu->width < 280 );
+	CHECK( menu->height == height );
+	return ret;
+}
+
 static int check_layout( void )
 {
 	int ret = 0;
 	char str[256];
 	float width, block_size;
 	LCUI_Widget child, box;
+
 	block_size = BOX_SIZE + (5 + 1) * 2;
 	box = LCUIWidget_GetById( "box-container" );
 	CHECK( box->x == 0 && box->y == 0 );
@@ -35,6 +48,7 @@ static int check_layout( void )
 		 child->x, child->y, width, 5. );
 	CHECK_WITH_TEXT( str, child->x == width && child->y == 5. );
 
+	ret += check_dropdown_menu();
 	return ret;
 }
 
