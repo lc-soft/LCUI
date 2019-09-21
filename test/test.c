904fc6b6d2476dbf6f87e9fb0f4cec61df0af03e
Liu Chao <lc-soft@live.cn>
2019-09-21 10:08:14 +0800

    test: exclude test_widget() from test.c

--- test/test.c
+++ test/test.c
@@ -18,7 +18,6 @@ int main(void)
 	ret += test_image_reader();
 	ret += test_css_parser();
 	ret += test_xml_parser();
-	ret += test_widget();
 	ret += test_widget_layout();
 	ret += test_widget_flex_layout();
 	ret += test_widget_inline_block_layout();
