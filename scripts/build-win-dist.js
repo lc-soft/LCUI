2929ea6e7eeb69b5b17a5f772cfeaf1eaf3bb4cd
Liu <lc-soft@live.cn>
2019-12-01 19:38:08 +0800

    build(scripts): remove build-win-dist.js

--- scripts/build-win-dist.js
+++
@@ -1,7 +0,0 @@
-const Builder = require('./builder')
-const config = require('./build-win-dist.config')
-const pkg = require('../package.json')
-
-const builder = new Builder(pkg, config)
-
-builder.run()
