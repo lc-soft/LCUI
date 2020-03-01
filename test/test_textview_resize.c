090ee414923835abe640b4b86c10101b96ff9c9c
Liu <lc-soft@live.cn>
2020-03-01 20:12:57 +0800

    test: update test_textview_resize.c

--- test/test_textview_resize.c
+++ test/test_textview_resize.c
@@ -161,9 +161,9 @@ int main(void)
 	LCUI_Init();
 
 	build();
-	LCUI_SetTimeout(6000, test_textview_set_text, NULL);
-	LCUI_SetTimeout(10000, test_textview_set_short_content_css, NULL);
-	LCUI_SetTimeout(15000, test_textview_set_long_content_css, NULL);
+	LCUI_SetTimeout(2000, test_textview_set_text, NULL);
+	LCUI_SetTimeout(4000, test_textview_set_short_content_css, NULL);
+	LCUI_SetTimeout(6000, test_textview_set_long_content_css, NULL);
 
 	return LCUI_Main();
 }
