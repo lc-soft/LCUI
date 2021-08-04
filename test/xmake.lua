24887d6318248c8d08ef118ea38555aa73667459
Liu <lc-soft@live.cn>
2021-08-04 23:25:58 +0800

    build: add test coverage report

--- test/xmake.lua
+++ test/xmake.lua
@@ -1,25 +1,21 @@
-includes("lib/test/xmake.lua")
-set_rundir("$(projectdir)/test")
+add_rules("mode.debug", "mode.release")
+set_rundir("$(projectdir)")
 add_repositories("local-repo ../build")
-add_requires("lcui")
+add_requires("lcui", "test")
 add_packages("lcui")
 add_includedirs("lib/test/include/")
 
-target("run_tests")
-    add_files("run_tests.c", "cases/*.c")
-    add_deps("test")
-
 target("helloworld")
     add_files("helloworld.c")
-    add_deps("test")
+    add_packages("test")
 
 target("test_block_layout")
     add_files("test_block_layout.c")
-    add_deps("test")
+    add_packages("test")
 
 target("test_border")
     add_files("test_border.c")
-    add_deps("test")
+    add_packages("test")
 
 target("test_box_shadow")
     add_files("test_box_shadow.c")
@@ -35,7 +31,7 @@ target("test_fill_rect_with_rgba")
 
 target("test_flex_layout")
     add_files("test_flex_layout.c")
-    add_deps("test")
+    add_packages("test")
 
 target("test_image_scaling_bench")
     add_files("test_image_scaling_bench.c")
@@ -63,14 +59,14 @@ target("test_scaling_support")
 
 target("test_scrollbar")
     add_files("test_scrollbar.c")
-    add_deps("test")
+    add_packages("test")
 
 target("test_string_render")
     add_files("test_string_render.c")
 
 target("test_textview_resize")
     add_files("test_textview_resize.c")
-    add_deps("test")
+    add_packages("test")
 
 target("test_touch")
     add_files("test_touch.c")
@@ -80,7 +76,7 @@ target("test_widget")
 
 target("test_widget_opacity")
     add_files("test_widget_opacity.c")
-    add_deps("test")
+    add_packages("test")
 
 target("test_widget_render")
     add_files("test_widget_render.c")
--- xmake.lua
+++ xmake.lua
@@ -1,6 +1,6 @@
 set_project("lcui")
 set_version("2.2.1")
-add_rules("mode.debug", "mode.release", "c++.openmp")
+add_rules("mode.debug", "mode.release", "c++.openmp", "mode.coverage")
 add_includedirs("include")
 add_rpathdirs("@loader_path/lib", "@loader_path")
 add_defines("LCUI_EXPORTS", "_UNICODE")
@@ -43,3 +43,25 @@ target("lcui")
         "lcui-timer",
         "lcui-worker"
     )
+
+target("run-tests")
+    set_kind("binary")
+    set_rundir("$(projectdir)/test")
+    set_default(false)
+    if is_plat("linux") and is_mode("coverage") then
+        on_run(function (target)
+            import("core.base.option")
+            local argv = {}
+            local options = {{nil, "memcheck",  "k",  nil, "enable memory check."}}
+            local args = option.raw_parse(option.get("arguments") or {}, options)
+            if args.memcheck then
+                table.insert(argv, "--leak-check=full")
+                table.insert(argv, "--error-exitcode=42")
+            end
+            table.insert(argv, val("projectdir").."/"..target:targetfile())
+            os.cd("$(projectdir)/test")
+            os.execv("valgrind", argv)
+        end)
+    end
+    add_files("./test/run_tests.c", "test/cases/*.c")
+    add_deps("test", "lcui")
