e246cc7dc5bceb30e7314c853ccc7923cb85d267
Liu <lc-soft@live.cn>
2021-08-04 23:25:58 +0800

    build: add test coverage report

--- test/run_tests.c
+++ test/run_tests.c
@@ -1,7 +1,7 @@
 ﻿#include <stdio.h>
 #include <wchar.h>
 #include <LCUI.h>
-#include "./lib/test/include/test.h"
+#include "../lib/test/include/test.h"
 #include "run_tests.h"
 
 int tests_count = 0;
