5b8d80de851975230bec96241f157e32807ff734
Liu <lc-soft@live.cn>
2021-08-07 20:20:29 +0800

    build: update options and configvars

--- lib/font/xmake.lua
+++ lib/font/xmake.lua
@@ -1,14 +1,5 @@
-option("with-freetype")
-    set_showmenu(true)
-    set_default(true)
-    set_configvar("USE_FREETYPE", 1)
-option_end()
-
-option("with-fontconfig")
-    set_showmenu(true)
-    set_default(true)
-    set_configvar("USE_FONTCONFIG", 1)
-option_end()
+option("with-freetype", {showmenu = true, default = true})
+option("with-fontconfig", {showmenu = true, default = true})
 
 if has_config("with-freetype") then
     add_requires("freetype", {optional = true})
@@ -26,3 +17,9 @@ target("lcui-font")
     add_options("with-freetype", "with-fontconfig")
     add_packages("freetype", "fontconfig")
     add_deps("lcui-util", "lcui-paint")
+    if has_package("fontconfig") then
+        set_configvar("USE_FONTCONFIG", 1)
+    end
+    if has_package("freetype") then
+        set_configvar("USE_FREETYPE", 1)
+    end
