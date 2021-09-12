a661433a3c5be29d75adc6e7ecb03670325e1538
Liu <lc-soft@live.cn>
2021-09-12 15:13:29 +0800

    build: always use the libxml2 remote package

--- lib/ui-builder/xmake.lua
+++ lib/ui-builder/xmake.lua
@@ -5,7 +5,7 @@ option("with-libxml2")
 option_end()
 
 if has_config("with-libxml2") then
-    add_requires("libxml2", {optional = true})
+    add_requires("libxml2", {optional = true, system = false})
 end
 
 target("lcui-ui-builder")
