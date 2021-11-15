87c3bcbbdab15065b98de7cab4573e9fda40a53b
WhoAteDaCake <doitsimplefy@gmail.com>
2021-11-15 21:38:07 +0000

    feat(clipboard): add windows implementation

---
+++ lib/platform/src/windows/windows_clipboard.c
@@ -0,0 +1,52 @@
+/*
+ * windows_clipboard.c -- clipboard support for windows platform.
+ *
+ * Copyright (c) 2018, Liu chao <lc-soft@live.cn> All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *   * Redistributions of source code must retain the above copyright notice,
+ *     this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in the
+ *     documentation and/or other materials provided with the distribution.
+ *   * Neither the name of LCUI nor the names of its contributors may be used
+ *     to endorse or promote products derived from this software without
+ *     specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <LCUI_Build.h>
+#ifdef LCUI_BUILD_IN_WIN32
+#include <LCUI/LCUI.h>
+#include <LCUI/clipboard.h>
+
+void LCUI_UseClipboard(LCUI_ClipboardAction action, void *arg)
+{
+}
+
+void LCUI_SetClipboardText(wchar_t *text, size_t len)
+{
+}
+
+void LCUI_InitClipboardDriver(void)
+{
+}
+
+void LCUI_FreeClipboardDriver(void)
+{
+}
+
+#endif
