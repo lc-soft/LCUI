48765c5ad66a7b5301ae45395f25ba202649bac3
Liu <lc-soft@live.cn>
2024-01-03 21:06:22 +0800

    build: set cairo as optional

--- examples/fabric/xmake.lua
+++ examples/fabric/xmake.lua
@@ -1,6 +1,9 @@
-add_requires("cairo")
+add_requires("cairo", { optional = true })
 
 target("fabric")
     set_kind("binary")
     add_files("src/*.c")
     add_packages("cairo", "lcui")
+    if has_package("cairo") then
+        add_defines("HAS_CAIRO")
+    end
