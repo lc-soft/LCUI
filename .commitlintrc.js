41b5262c00c07ce72d623bb7629ea7f3f822dcd9
Liu <lc-soft@live.cn>
2024-05-05 21:57:13 +0800

    chore: remove devDependencies

--- .commitlintrc.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  extends: ["@commitlint/config-conventional"],
-  rules: {
-    "header-min-length": [2, "always", 1],
-    "header-max-length": [2, "always", 72],
-    "subject-case": [0],
-  },
-};
