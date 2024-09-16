daec39a20e5086294f04db45794f311a872bceb2
Liu <lc-soft@live.cn>
2024-09-16 22:24:18 +0800

    test: update mainloop tests

--- tests/cases/test_mainloop.c
+++ tests/cases/test_mainloop.c
@@ -15,67 +15,67 @@
 
 static void handle_refresh(void *arg)
 {
-	ui_refresh_style();
+        ui_refresh_style();
 }
 
 static void handle_quit(void *arg)
 {
-	lcui_quit();
+        lcui_quit();
 }
 
-static void handle_btn_click(ui_widget_t* w, ui_event_t* ui_event, void *arg)
+static void handle_btn_click(ui_widget_t *w, ui_event_t *ui_event, void *arg)
 {
-	lcui_set_timeout(0, handle_refresh, NULL);
-	lcui_set_timeout(1, handle_quit, NULL);
-	lcui_process_events(APP_PROCESS_EVENTS_UNTIL_QUIT);
-	lcui_quit();
+        lcui_set_timeout(0, handle_refresh, NULL);
+        lcui_set_timeout(1, handle_quit, NULL);
+        lcui_process_events(APP_PROCESS_EVENTS_UNTIL_QUIT);
+        lcui_quit();
 }
 
 static void handle_trigger_btn_click(void *arg)
 {
-	app_event_t e = { 0 };
+        app_event_t e = { 0 };
 
-	e.type = APP_EVENT_MOUSEDOWN;
-	e.mouse.button = MOUSE_BUTTON_LEFT;
-	e.mouse.x = 5;
-	e.mouse.y = 5;
-	app_post_event(&e);
-	e.type = APP_EVENT_MOUSEUP;
-	app_post_event(&e);
+        e.type = APP_EVENT_MOUSEDOWN;
+        e.mouse.button = MOUSE_BUTTON_LEFT;
+        e.mouse.x = 5;
+        e.mouse.y = 5;
+        app_post_event(&e);
+        e.type = APP_EVENT_MOUSEUP;
+        app_post_event(&e);
 }
 
 static void observer_thread(void *arg)
 {
-	int i;
-	bool *exited = arg;
+        int i;
+        bool *exited = arg;
 
-	for (i = 0; i < 100 && !*exited; ++i) {
-		sleep_ms(1);
-	}
-	ctest_equal_bool("main loop should exit within 100ms", *exited, true);
-	if (!*exited) {
-		exit(-ctest_finish());
-		return;
-	}
-	thread_exit(NULL);
+        for (i = 0; i < 10 && !*exited; ++i) {
+                sleep_ms(100);
+        }
+        ctest_equal_bool("main loop should exit within 1000ms", *exited, true);
+        if (!*exited) {
+                exit(-ctest_finish());
+                return;
+        }
+        thread_exit(NULL);
 }
 
 void test_mainloop(void)
 {
-	thread_t tid;
-	ui_widget_t *btn;
-	bool exited = false;
+        thread_t tid;
+        ui_widget_t *btn;
+        bool exited = false;
 
-	lcui_init();
-	btn = ui_create_widget("button");
-	ui_button_set_text(btn, "button");
-	ui_widget_on(btn, "click", handle_btn_click, NULL);
-	ui_root_append(btn);
-	/* Observe whether the main loop has exited in a new thread */
-	thread_create(&tid, observer_thread, &exited);
-	/* Trigger the click event after the first frame is updated */
-	lcui_set_timeout(50, handle_trigger_btn_click, btn);
-	lcui_main();
-	exited = true;
-	thread_join(tid, NULL);
+        lcui_init();
+        btn = ui_create_widget("button");
+        ui_button_set_text(btn, "button");
+        ui_widget_on(btn, "click", handle_btn_click, NULL);
+        ui_root_append(btn);
+        /* Observe whether the main loop has exited in a new thread */
+        thread_create(&tid, observer_thread, &exited);
+        /* Trigger the click event after the first frame is updated */
+        lcui_set_timeout(50, handle_trigger_btn_click, btn);
+        lcui_main();
+        exited = true;
+        thread_join(tid, NULL);
 }
