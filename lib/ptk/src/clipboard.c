a9ab79a338cf33dc81e8e73265e277d80768c1ef
Liu <lc-soft@live.cn>
2025-08-16 10:40:32 +0800

    fix(ptk): request text failed when clipboard content is empty (#318)

--- lib/ptk/src/clipboard.c
+++ lib/ptk/src/clipboard.c
@@ -16,72 +16,76 @@
 #include "linux/x11clipboard.h"
 
 static struct ptk_clipboard_module {
-	char *text;
-	size_t text_len;
+        char *text;
+        size_t text_len;
 } ptk_clipboard;
 
 int ptk_clipboard_request_text(ptk_clipboard_callback_t callback, void *arg)
 {
 #ifdef PTK_HAS_LIBX11
-	if (ptk_get_app_id() == PTK_APP_ID_LINUX_X11) {
-		return ptk_x11clipboard_request_text(callback, arg);
-	}
+        if (ptk_get_app_id() == PTK_APP_ID_LINUX_X11) {
+                return ptk_x11clipboard_request_text(callback, arg);
+        }
 #endif
-	size_t len = ptk_clipboard.text_len + 1;
-	wchar_t *wstr = malloc(sizeof(wchar_t) * len);
-	ptk_clipboard_t clipboard = { 0 };
+        size_t len = ptk_clipboard.text_len + 1;
+        wchar_t *wstr = malloc(sizeof(wchar_t) * len);
+        ptk_clipboard_t clipboard = { 0 };
 
-	len = decode_utf8(wstr, ptk_clipboard.text, len);
-	wstr[len] = 0;
-	// Assign the data
-	clipboard.text = wstr;
-	clipboard.len = len;
-	clipboard.image = NULL;
-	callback(&clipboard, arg);
-	free(wstr);
-	return 0;
+        if (len > 1) {
+                len = decode_utf8(wstr, ptk_clipboard.text, len);
+                wstr[len] = 0;
+        } else {
+                wstr[0] = 0;
+        }
+        // Assign the data
+        clipboard.text = wstr;
+        clipboard.len = len;
+        clipboard.image = NULL;
+        callback(&clipboard, arg);
+        free(wstr);
+        return 0;
 }
 
 int ptk_clipboard_set_text(const wchar_t *text, size_t len)
 {
 #ifdef PTK_HAS_LIBX11
-	if (ptk_get_app_id() == PTK_APP_ID_LINUX_X11) {
-		return ptk_x11clipboard_set_text(text, len);
-	}
+        if (ptk_get_app_id() == PTK_APP_ID_LINUX_X11) {
+                return ptk_x11clipboard_set_text(text, len);
+        }
 #endif
-	size_t raw_len = encode_utf8(NULL, text, 0);
-	char *raw_text = malloc((raw_len + 1) * sizeof(char));
-	raw_len = encode_utf8(raw_text, text, raw_len);
+        size_t raw_len = encode_utf8(NULL, text, 0);
+        char *raw_text = malloc((raw_len + 1) * sizeof(char));
+        raw_len = encode_utf8(raw_text, text, raw_len);
 
-	if (ptk_clipboard.text) {
-		free(ptk_clipboard.text);
-	}
-	raw_text[raw_len] = '\0';
-	ptk_clipboard.text = raw_text;
-	ptk_clipboard.text_len = raw_len;
-	return 0;
+        if (ptk_clipboard.text) {
+                free(ptk_clipboard.text);
+        }
+        raw_text[raw_len] = '\0';
+        ptk_clipboard.text = raw_text;
+        ptk_clipboard.text_len = raw_len;
+        return 0;
 }
 
 void ptk_clipboard_init(void)
 {
 #ifdef PTK_HAS_LIBX11
-	if (ptk_get_app_id() == PTK_APP_ID_LINUX_X11) {
-		ptk_x11clipboard_init();
-		return;
-	}
+        if (ptk_get_app_id() == PTK_APP_ID_LINUX_X11) {
+                ptk_x11clipboard_init();
+                return;
+        }
 #endif
 }
 
 void ptk_clipboard_destroy(void)
 {
 #ifdef PTK_HAS_LIBX11
-	if (ptk_get_app_id() == PTK_APP_ID_LINUX_X11) {
-		ptk_x11clipboard_destroy();
-		return;
-	}
+        if (ptk_get_app_id() == PTK_APP_ID_LINUX_X11) {
+                ptk_x11clipboard_destroy();
+                return;
+        }
 #endif
-	if (ptk_clipboard.text) {
-		free(ptk_clipboard.text);
-	}
-	ptk_clipboard.text = NULL;
+        if (ptk_clipboard.text) {
+                free(ptk_clipboard.text);
+        }
+        ptk_clipboard.text = NULL;
 }
